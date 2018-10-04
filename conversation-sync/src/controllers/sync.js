7630a7b13865f2fc93f8e10b769b35636fc9fae4
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2018-10-04 19:49:46 +0800

    Initial skeleton

---
+++ conversation-sync/src/controllers/sync.js
@@ -0,0 +1,28 @@
+class Sync {
+	async processGuest(payload) {
+		// Format dates
+
+		// Sync Kepla
+			// Find / Create Facil record
+			// Find / Create Host
+			// Create Participant
+				// Create participant
+				// Find facil user
+				// Assign participant to facil user
+			// Find / Create conversation
+				// Assign host to conversation
+				// Assign facil to conversation
+			// Assign participant to conversation
+			// Assign facil user to conversation
+
+			// Mark record kepla synced
+
+		// Sync FTL
+			// Find / create facil
+			// Find / create host
+			// Find / create conversation
+			// Find / create guests
+
+			// Mark record FTL synced
+	}
+}
