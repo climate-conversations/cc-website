fc480a04db64ef1cbfbaceb0137ab93a6f579685
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2018-10-09 21:06:19 +0800

    Set messaging permissions, allow user not to be found

--- conversation-sync/src/controllers/kepla.js
+++ conversation-sync/src/controllers/kepla.js
@@ -9,8 +9,15 @@ async function syncGuestToKepla(data) {
 	dateKeys.forEach((key) => { data[key] = data[key] ? isoDateToKeplaDate(data[key]) : null; });
 	data.residentialstatus = kepla.mapToKeplaResidency(data.residentialstatus);
 
+	if (!data.participantemail) {
+		console.log(`KEPLA: no email for ${data.participantname}, skipping`);
+		return null;
+	}
+
+	console.log(`KEPLA: Upserting ${data.hostname} (host), ${data.facilitatorname} (facil), ${data.participantname} (guest)`)
 	/* eslint-disable quote-props */
-	const [host, facilitator, guest, facilUser] = await Promise.all([
+	// eslint-disable-next-line prefer-const
+	let [host, facilitator, guest, facilUser] = await Promise.all([
 		kepla.upsertPerson(data.hostemailaddress, {
 			'Email': data.hostemailaddress,
 			'Full Name': data.hostname,
@@ -31,21 +38,34 @@ async function syncGuestToKepla(data) {
 			'Facilitate': data.facilitate,
 			'Volunteer': data.volunteer,
 			'Take 2 hours with friends': data.give2hourstoclimateactionwfriends,
-		}),
+		}, ['Email', 'Mobile Phone']),
 		kepla.findUser(data.facilitatoremailaddress),
 	]);
 	/* eslint-enable quote-props */
 
 	const conversationDetails = { host, facilitator, status: 'Completed' };
 	conversationDetails.date = data.conversationdate;
-	const conversation = await kepla.upsertConversation(conversationDetails);
+	let conversation = await kepla.upsertConversation(conversationDetails);
+
+	[conversation, guest] = await Promise.all([
+		kepla.getRecord(conversation), kepla.getRecord(guest),
+	]);
+
+	const promises = [
+		kepla.addRelationship(conversation, guest, 'attendee'),
+	];
+
+	if (facilUser) {
+		promises.concat([
+			kepla.assignUserToRecord(facilUser, guest, 'person'),
+			kepla.assignUserToRecord(facilUser, conversation, 'conversation'),
+		]);
+	} else {
+		console.log(`WARNING: No facilitator user found for ${data.facilitatorname} records are unassigned`);
+	}
 
 	// Assign facilitator to records
-	return Promise.all([
-		kepla.assignUserToRecord(facilUser, guest, 'person'),
-		kepla.assignUserToRecord(facilUser, conversation, 'conversation'),
-		kepla.addRelationship(conversation, guest, 'attendee'),
-	]);
+	return Promise.all(promises);
 }
 
 module.exports = { syncGuestToKepla };
