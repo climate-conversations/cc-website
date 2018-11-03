5efccc9d3a7bfad45742616c807290931260d0b2
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2018-11-03 22:55:58 +0800

    Set up deploy script

---
+++ conversation-sync/Gruntfile.js
@@ -0,0 +1,91 @@
+const childProcess = require('child_process');
+const readline = require('readline');
+const _ = require('lodash');
+
+const routes = require('./config/routes');
+
+const gcloud = require('./config/gcloud');
+
+const GOOGLE_DEFAULTS = {
+	memory: '128MB',
+	runtime: 'nodejs8',
+};
+
+const googleConfig = Object.assign(GOOGLE_DEFAULTS, gcloud);
+
+const functionDeployArgs = _.flatten(_.map(googleConfig).map((key, val) =>
+	[`--${_.kebabCase(key)}`, val]));
+
+module.exports = (grunt) => {
+	grunt.registerMultiTask('airblast', 'Deploy cloud functions', function airblast() {
+		if (this.target !== 'deploy') {
+			throw new Error(`Unknown target ${this.target}`);
+		}
+
+		const done = this.async();
+
+		const promises = routes.map(deployRoute);
+
+		Promise.all(promises).then(() => done()).catch(done);
+	});
+	grunt.initConfig({
+		airblast: {
+			deploy: {},
+		},
+	});
+};
+
+async function deployRoute(route) {
+	const command = 'gcloud';
+	const args = [];
+	let description;
+
+	if (route.type === 'http') {
+		args.push('functions', 'deploy', route.path);
+		args.push(...functionDeployArgs);
+		args.push('--trigger-http');
+		description = `HTTP function ${route.path}`;
+	} else if (route.type === 'pubsub') {
+		args.push('functions', 'deploy', route.path, '--trigger-resource');
+		args.push(...functionDeployArgs);
+		args.push(route.topic, '--trigger-event', 'google.pubsub.topic.publish');
+		description = `pubsub function ${route.path} on topic ${route.topic}`;
+	} else {
+		throw new Error(`Unknown route type ${route.type}`);
+	}
+
+	console.log(`Deploying ${description}`);
+
+	try {
+		await spawnChild(command, args, { env: process.env, shell: true });
+	} catch (err) {
+		console.log('Deploy command failed:');
+		console.log(`  ${command} ${args.join(' ')}`);
+		console.log('Output:');
+		console.log(err.allout);
+		throw err;
+	}
+}
+
+async function spawnChild(command, args, opts) {
+	const stderr = [];
+	const stdout = [];
+	const allout = [];
+	return new Promise((resolve, reject) => {
+		const child = childProcess.spawn(command, args, opts);
+		readline.createInterface({
+			input: child.stdout,
+		}).on('line', (line) => { stdout.push(line); allout.push(line); });
+		readline.createInterface({
+			input: child.stderr,
+		}).on('line', (line) => { stderr.push(line); allout.push(line); });
+		child.on('exit', (code) => {
+			const output = { stderr, stdout, allout };
+			if (code !== 0) {
+				const err = new Error(`${command} exited with code ${code}`);
+				err.output = output;
+				reject(err);
+			}
+		});
+	});
+}
