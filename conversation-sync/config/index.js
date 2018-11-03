369dc8633e843a5192fea426bd73dbf49dc75571
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2018-11-03 22:55:37 +0800

    Setup datastore-env

---
+++ conversation-sync/config/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const DatastoreEnvironment = require('datastore-env');
+const deasync = require('deasync');
+
+// File generated above
+const envVars = require('./requiredEnv.js');
+
+const env = new DatastoreEnvironment(envVars);
+
+// Wrap loadEnvironment in a node callback style
+function asyncLoad(cb) {
+	env.loadEnvironment().then(() => cb()).catch(cb);
+}
+
+// Block execution until loadEnvironment has finished
+deasync(asyncLoad)();
---
+++ conversation-sync/config/requiredEnv.js
@@ -0,0 +1,9 @@
+module.exports = {
+	required: [
+		'AIRTABLES_BASE',
+		'AIRTABLES_KEY',
+		'KEPLA_KEY',
+	],
+	optional: [
+	],
+};
