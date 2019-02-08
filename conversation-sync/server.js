2d367516607ab43b0c5b0445de370d90e5897766
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2019-02-08 10:41:23 +0800

    Log environment for debugging

--- conversation-sync/server.js
+++ conversation-sync/server.js
@@ -1,6 +1,10 @@
 require('./config');
 const routes = require('./config/routes');
 
+console.log('Environment:')
+Object.keys(process.env).forEach(key => console.log(`${key} ${process.env[key]}`));
+console.log(process.env);
+
 module.exports = {};
 
 routes.forEach((route) => { module.exports[route.path] = route.fn; });
