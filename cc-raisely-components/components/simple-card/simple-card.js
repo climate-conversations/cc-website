67e6dedb91a4ab46b19ff53d05396927ad1aa5e6
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2019-06-26 10:44:00 +0800

    Updates by luke

--- cc-raisely-components/components/simple-card/simple-card.js
+++ cc-raisely-components/components/simple-card/simple-card.js
@@ -1,6 +1,22 @@
-(RaiselyComponents) => class MyRaiselyComponent extends React.Component {
+(RaiselyComponents) => class SimpleCard extends React.Component {
 	render() {
-		const values = this.props.getValues();
-		return <h1>This is a boilerplate Raisely custom component</h1>;
+		const card = this.props.getValues();
+     
+      
+		return <article className={`card card--${card.colour}${card.link ? ' card--link' : ''}`}>
+                    
+          {card.link && <a className="card__link" href={card.link}/>}
+          
+          {card.title && <div className="card__title">
+            <h4>{card.title}</h4>
+            {card.link && <i className="material-icons">arrow_forward</i>}
+          </div>}
+          
+          {(card.image || card.title) && <div className="card__content">
+            {card.image && <div className="card__content-image" style={{ backgroundImage: `url(${card.image}` }}/>}
+            {card.text && <div className="card__content-text">{card.text}</div>}
+          </div>}
+          
+        </article>;
 	}
 }
