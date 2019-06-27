35aa4cfaa3d25c9b6b9c93346d35002cdba3cce6
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2019-06-27 15:16:05 +0800

    More fully implement facil processing checklist

--- cc-raisely-components/components/facil-processing-checklist/facil-processing-checklist.js
+++ cc-raisely-components/components/facil-processing-checklist/facil-processing-checklist.js
@@ -1,22 +1,20 @@
 (RaiselyComponents, React) => {
+	const { Link } = RaiselyComponents;
 	const { Icon } = RaiselyComponents.Atoms;
 
 	// These functions determine when to mark an item as done
 	async function atLeastOneGuest(conversation) {
 		// Fetch rsvps, find one with a survey
 	}
-	const hasPhoto = (conversation) => !!conversation.private.photoUrl;
+	const hasPhoto = conversation => !!conversation.private.photoUrl;
 	async function hasReflection(conversation) {
 		// Load facils and co-facils
 		// Check they have complete reflections
 	}
 	function donationsReported(conversation) {
-		conversation.private.wereDonationsReceived === false ||
-		conversations.private.cashTransferReference
+		conversation.private.cashReceived === false ||
+		conversation.private.cashTransferReference;
 	}
-
-	const cashReport = ['event.wereDonationsReceived', 'event.cashReceived', 'event.reportScan'];
-	const transferReport = ['event.transferScreenshot', 'event.cashTransferredAt', 'event.cashTransferReference'];
 
 	const checklist = [
 		{ id: 'reflection', label: 'Complete a reflection', href: '/conversations/reflection', done: hasReflection },
@@ -26,28 +24,64 @@
 		{ id: 'email-guests', label: 'Email Guests', href: '/conversations/email-guests' },
 		{ id: 'host-report', label: 'Send Host Report', href: '/conversations/host-report' },
 		{ id: 'destroy-surveys', label: 'Destroy Surveys', href: '/conversations/destroy-surveys' },
-	]
+	];
 
-	class ChecklistItem extends React.Component {
-		render() {
-			const { label, isDone, href } = this.props.item;
-			const icon = isDone ? 'done' : 'check-circle-outline';
+	function CheckListItem({ item }) {
+		const { label, isDone, href } = item;
+		const icon = isDone ? 'done' : 'check-circle-outline';
 
-			return (
-				<div>
-					<Icon name={icon} href={href} />
-					<Link href={link}>{label}</Link>
-				</div>
-			);
-		}
+		return (
+			<div>
+				<Icon name={icon} href={href} />
+				<Link href={href}>{label}</Link>
+			</div>
+		);
 	}
 
 	return class FacilProcessingChecklist extends React.Component {
 		componentDidMount() {
-			// Fetch the conversation
-			// For each checklist, figure out if it's done
-			// update conversation.private.processChecklist
-			// update conversation.private.isProcessed
+		}
+
+		async load() {
+			let records;
+			try {
+				records = await loadPageRecords({ props: this.props, require: ['event'] });
+			} catch (e) {
+				console.error(e);
+				this.setState(e.message);
+			}
+
+			const { event: conversation } = records;
+
+			const completedSteps = (conversation.private.completedSteps || '').split(';').sort();
+
+			// Update any if they are now done
+			checklist.forEach(async (item) => {
+				if (!completedSteps.includes(item.id)) {
+					if (item.done) {
+						item.isDone = await item.done(conversation);
+					}
+				} else {
+					item.isDone = true;
+				}
+			});
+
+			// Get a list of now completed steps
+			const nowComplete = checklist
+				.map(i => (i.isDone ? 'id' : null))
+				.filter(i => i)
+				.sort();
+
+			const stepsDoneNow = nowComplete.join(';');
+
+			const hasChanged = (stepsDoneNow === completedSteps.join(';'));
+
+			if (hasChanged) {
+				conversation.private.completedSteps = stepsDoneNow();
+				const allDone = checklist.reduce((i, done) => (i.isDone && done), true);
+				if (allDone) conversation.private.isProcessed = true;
+				// Update API
+			}
 		}
 
 		render() {
@@ -56,5 +90,5 @@
 				<CheckListItem item={item} />
 			));
 		}
-	}
-}
+	};
+};
