e31c287fb9e5acda603a0460b549d4c76c8cc537
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2019-07-11 13:36:10 +0800

    Update simple card

--- cc-raisely-components/components/simple-card/simple-card.js
+++ cc-raisely-components/components/simple-card/simple-card.js
@@ -3,7 +3,7 @@
 		const card = this.props.getValues();
      
       
-		return <article className={`card card--${card.colour}${card.link ? ' card--link' : ''}`}>
+		return <article className={`card card--${card.colour}${card.link ? ' card--link' : ''} ${card.image && 'card--has-image'}`}>
                     
           {card.link && <a className="card__link" href={card.link}/>}
           
@@ -12,7 +12,7 @@
             {card.link && <i className="material-icons">arrow_forward</i>}
           </div>}
           
-          {(card.image || card.title) && <div className="card__content">
+          {(card.image || card.text) && <div className="card__content">
             {card.image && <div className="card__content-image" style={{ backgroundImage: `url(${card.image}` }}/>}
             {card.text && <div className="card__content-text">{card.text}</div>}
           </div>}
