17da10efcd4e6a7b74ba4298f9552fc42dcf4297
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2019-08-09 23:12:37 +0800

    Complete host list component

--- cc-raisely-components/components/facil-host-list/facil-host-list.js
+++ cc-raisely-components/components/facil-host-list/facil-host-list.js
@@ -1,7 +1,7 @@
 (RaiselyComponents, React) => {
 	const { Icon, Button } = RaiselyComponents.Atoms;
 	const { get, dayjs } = RaiselyComponents.Common;
-	const { api } = RaiselyComponents;
+	const { api, Spinner, Link } = RaiselyComponents;
 
 	const icons = {
 		public: 'public',
@@ -9,68 +9,92 @@
 		corporate: 'accessibility_new',
 	};
 
+	async function getData(promise) {
+		const response = await promise;
+		const status = response.statusCode();
+		if (status >= 400) {
+			const message = get(response.body().data(), 'errors[0].message', 'An unknown error has occurred');
+			console.error(response.body());
+			throw new Error(message);
+		}
+		return response.body().data().data;
+	}
+
 	class Host extends React.Component {
 		componentDidMount() {
-			this.setTimes();
-		}
-
-		setTimes() {
-			// eslint-disable-next-line object-curly-newline
-			const { cashDonationsNotes, cashTransferAmount, cashReceivedAmount, cashCtaAmount, processAt } = get(this.props, 'conversation.private', {});
-
-			const startAt = get(this.props, 'conversation.startAt');
-
-			this.processOverdue = processAt.add(1, 'day');
-			this.startAt = dayjs(startAt);
-			this.displayDate = this.startAt.format('DD/MM/YYYY');
-
-			this.reconciled = cashDonationsNotes ||
-				((cashTransferAmount === cashReceivedAmount) && (cashTransferAmount === cashCtaAmount));
 		}
 
 		render() {
-			const { now, conversation, showFacil } = this.props;
-
-			const { conversationType, isProcessed } = get(conversation, 'private', {});
-
-			const overdue = this.processOverdue.isBefore(now) && !isProcessed;
-			const hasPassed = this.startAt.isBefore(now);
-
-			const warning = overdue || !this.reconciled;
-
-			const icon = (warning ? 'warning' : icons[conversationType]) || icons.private;
+			const { host, showFacil } = this.props;
+			const url = `/hosts/${host.userUuid}`;
+			let conversationName;
+			if (host.conversation) conversationName = (host.conversation === true) ?
+				'...' : host.conversation.name
 
 			return (
-				<li className="conversation-list-item" key={conversation.uuid}>
-					<Icon name={icon} />
-					<div className="conversation-start">{this.displayDate}</div>
-					<div className="conversation-name">{conversation.name}</div>
-					{showFacil ? <div className="conversation-facil">{conversation.name}</div> : ''}
-					<Button>edit</Button>
-					{hasPassed && !isProcessed ? <Button>process</Button> : ''}
+				<li className="" key={host.uuid}>
+					<Link className="list__item host-list-item" href={url}>
+						<div className="list__item--title">
+							{host.user.fullName}
+							<div className="list__item--subtitle">
+								{host.conversation ? conversationName : '(no conversation)'}
+								{showFacil ? <div className="host-facil"> - {host.facilitator.preferredName}</div> : ''}
+							</div>
+						</div>
+						<div className="host-status">{host.private.status}</div>
+						<Button>contact</Button>
+					</Link>
 				</li>
 			);
 		}
 	}
 
 	return class FacilHostList extends React.Component {
-		state = { filter: true };
+		state = { filter: true, loading: true };
 
 		componentDidMount() {
+			// Cache conversations and facilitators by uuid
+			this.conversations = {};
+			this.facilitators = {};
 			this.load();
 		}
 
 		setHosts = () => {
-			const hostStatus = ['lead', 'interested', 'booked', 'complete', 'not interested'];
-			const filterStatus = ['lead', 'interested'];
+			const hostStatus = ['lead', 'interested', 'booked', 'hosted', 'not interested'];
+			const filterStatus = ['lead', 'interested', 'booked'];
+			const isTeam = this.isTeam();
 
 			let { hosts } = this;
 			if (this.state.filter) {
 				hosts = this.hosts
-					.filter(h => filterStatus.includes(get(h, 'private.status')));
+					.filter(h => filterStatus.includes(get(h, 'private.status')))
+					// Ensure visible hosts have conversation
+					.forEach((host) => {
+						if (!host.conversation) {
+							const uuid = get(host, 'private.conversationUuid');
+							if (uuid) {
+								// Multiple hosts may come from the same conversation
+								// so cache the promise to retrieve so we only fetch it
+								// once, and add a then callback to the promise for each host
+								if (!this.conversations[uuid]) {
+									this.conversations[uuid] = getData(api.events.get({ id: uuid }))
+										.catch(e => this.setState({ error: e }));
+									// eslint-disable-next-line no-param-reassign
+									host.conversation = true;
+								}
+								this.conversations[uuid]
+									// eslint-disable-next-line no-param-reassign
+									.then((c) => { host.conversation = c; });
+							}
+						}
+						if (isTeam && !host.facilitator) {
+							// eslint-disable-next-line no-param-reassign
+							host.facilitator = this.facilitators[host.private.facilitatorUuid];
+						}
+					});
 			}
 
-			this.setState({ hosts });
+			this.setState({ hosts, loading: false });
 		}
 
 		async getUserUuids() {
@@ -78,29 +102,52 @@
 			let uuids = currentUserUuid;
 
 			if (this.isTeam()) {
-				uuids = await api.users.getAll({
+				const facilitators = await getData(api.users.getAll({
 					query: {
 						'private.teamLeaderUuid': currentUserUuid,
 					},
-				});
+				}));
+				uuids = facilitators
+					.map((f) => {
+						// Save the facilitator in the uuid map
+						this.facilitators[f.uuid] = f;
+						// Map it to it's uuid to create the query param
+						return f.uuid;
+					})
+					.join(',');
 			}
 
 			return uuids;
 		}
 
+		isTeam = () => {
+			const { show } = this.props.getValues();
+			return show === 'team';
+		}
+
 		async load() {
-			const userUuid = this.getUserUuids();
-			// const rsvps = await api.eventRsvps.getAll({
-			// 	query: {
-			// 		userUuid,
-			// 		type: 'facilitator,co-facilitator',
-			// 	},
-			// });
-			// this.conversations = rsvps.map((rsvp) => {
-			// 	// eslint-disable-next-line no-param-reassign
-			// 	rsvp.event.facilitator = rsvp.user;
-			// 	return rsvp.event;
-			// });
+			try {
+				const userUuid = await this.getUserUuids();
+				this.hosts = await getData(api.interactions.getAll({
+					query: {
+						private: 1,
+						category: 'host-interest',
+						facilitatorUuid: userUuid,
+						join: 'user',
+					},
+				}));
+
+				await Promise.all(this.hosts.map(async (host) => {
+					if (!host.user) {
+						// eslint-disable-next-line no-param-reassign
+						host.user = await getData(api.users.get({ id: host.userUuid }));
+					}
+				}));
+			} catch (error) {
+				this.setState({ error, loading: false });
+			}
+
+			this.setHosts();
 		}
 
 		toggleFilter = () => {
@@ -110,23 +157,35 @@
 
 		render() {
 			const now = dayjs();
-			const { conversations, filter } = this.state;
+			const { hosts, filter, loading, error } = this.state;
+
+			if (loading) return <Spinner />;
+
 			const isTeam = this.isTeam();
 
 			return (
-				<div className="ohst-list__wrapper">
-					<Button onClick={this.toggleFilter}>
-						{filter ? 'Show All' : 'Show Outstanding' }
-					</Button>
-					<ul className="host-list">
-						{conversations.map(conversation => (
-							<Host
-								{...this.props}
-								now={now}
-								showFacil={isTeam}
-								conversation={conversation} />
-						))}
-					</ul>
+				<div className="host-list__wrapper list__wrapper">
+					{error ? (
+						<div className="error">{error.message}</div>
+					) : ''}
+					{this.hosts.length ? (
+						<Button className="list__toggle" onClick={this.toggleFilter}>
+							{filter ? 'Show All' : 'Hide Complete' }
+						</Button>
+					) : ''}
+					{hosts.length ? (
+						<ul className="host-list">
+							{hosts.map(host => (
+								<Host
+									{...this.props}
+									now={now}
+									showFacil={isTeam}
+									host={host} />
+							))}
+						</ul>
+					) : (
+						<p>You have no {this.hosts.length ? 'unbooked' : ''} hosts</p>
+					)}
 				</div>
 			);
 		}
