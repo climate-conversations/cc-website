8bd97d337490846451c5f144ccc34a9967251580
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2019-08-12 15:34:28 +0800

    Helper for buttons that return to prior page

--- cc-raisely-components/components/return-button/return-button.js
+++ cc-raisely-components/components/return-button/return-button.js
@@ -1,6 +1,59 @@
-(RaiselyComponents) => class MyRaiselyComponent extends React.Component {
-	render() {
-		const values = this.props.getValues();
-		return <h1>This is a boilerplate Raisely custom component</h1>;
+(RaiselyComponents, React) => {
+	const { Button } = RaiselyComponents.Atoms;
+	const { getQuery } = RaiselyComponents.api;
+	const { get, qs } = RaiselyComponents.Common;
+
+	function parsePath(url) {
+		const split = url.indexOf('?');
+		const path = split === -1 ? url : url.slice(0, split);
+		const query = split === -1 ? '' : url.slice(split + 1, url.length);
+
+		return { path, query };
+	}
+
+	function mergeQuery(url, newQuery) {
+		const { path, query } = parsePath(url);
+
+		const fullQuery = qs.stringify(Object.assign(query, newQuery));
+		const finalUrl = `${path}?${fullQuery}`;
+		return finalUrl;
+	}
+
+	function getReturnUrl(query, defaultUrl, isDone) {
+		const { onDone, returnTo } = query;
+		const returnUrl = returnTo || defaultUrl;
+
+		if (!isDone) return returnUrl;
+
+		return mergeQuery(returnUrl, { done: onDone });
+	}
+
+	return class ReturnButton extends React.Component {
+		static createReturningLink({ props, url, done }) {
+			const { pathname, search } = get(props, 'router.location');
+			const thisPage = `${pathname}${search.startsWith('?') ? '' : '?'}${search}`;
+
+			return mergeQuery(url, {
+				returnTo: thisPage,
+				onDone: done,
+			});
+		}
+
+		render() {
+			const values = this.props.getValues();
+			const { backLabel, saveLabel } = values;
+			const defaultUrl = values.defaultUrl || '/dashboards';
+			const query = getQuery(get(this.props, 'router.location.search'));
+
+			const backUrl = getReturnUrl(query, defaultUrl);
+			const saveUrl = getReturnUrl(query, defaultUrl, true);
+
+			return (
+				<div>
+					{ backLabel ? <Button href={backUrl}>{backLabel}</Button> : '' }
+					{ saveLabel ? <Button href={saveUrl}>{saveLabel}</Button> : '' }
+				</div>
+			);
+		}
 	}
 }
