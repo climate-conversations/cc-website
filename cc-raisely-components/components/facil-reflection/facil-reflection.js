920da7e8978a1910bb174fcf181b7dfa23846d3d
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2019-08-16 20:00:55 +0800

    Fixes for facil reflection

--- cc-raisely-components/components/facil-reflection/facil-reflection.js
+++ cc-raisely-components/components/facil-reflection/facil-reflection.js
@@ -2,6 +2,7 @@
 (RaiselyComponents, React) => {
 	const CustomForm = RaiselyComponents.import('custom-form');
 	const { api } = RaiselyComponents;
+	const { get } = RaiselyComponents.Common;
 
 	return class FacilReflection extends React.Component {
 		generateForm() {
@@ -13,24 +14,27 @@
 		}
 
 		load = async ({ dataToForm }) => {
-			const eventUuid = this.props.eventUuid || this.props.router.match.params.event;
+			return;
+			const eventUuid = this.props.eventUuid || get(this.props, 'location.router.match.params.event');
 
 			const query = {
 				record: eventUuid,
 				recordType: 'event',
 				category: 'facil-reflection',
-				userUuid: this.props.globals.user.uuid,
+				user: get(this.props, 'globals.user.uuid'),
 			};
 
 			let interaction;
 
-			if (eventUuid) {
+			interaction = await api.interactions.get({
+				query,
+			});
+			if (!interaction) {
 				interaction = query;
-			} else {
-				// Load event and rsvps
-				interaction = await api.interactions.get({
-					query,
-				});
+				interaction.categoryUuid = interaction.category;
+				interaction.userUuid = interaction.user;
+				delete interaction.category;
+				delete interaction.user;
 			}
 
 			return dataToForm({ interaction });
@@ -38,18 +42,17 @@
 
 		async save(values, formToData) {
 			const data = formToData(values);
-
-			return api.upsert('events', { data: data.interactions['facil-reflection'] });
+			return api.upsert('interactions', { data: data.interactions['facil-reflection'] });
 		}
 
 		render() {
 			const config = this.generateForm();
 			return (<CustomForm
+				{...this.props}
 				steps={config}
 				controller={this}
 				followNextQuery="1"
-			/>
-			);
+			/>);
 		}
 	};
 };
