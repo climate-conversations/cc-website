dc0c7baedc4836eb8f84fb4ed46b843c40a02ba0
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2019-08-16 19:59:40 +0800

    replace a with link

--- cc-raisely-components/components/simple-card/simple-card.js
+++ cc-raisely-components/components/simple-card/simple-card.js
@@ -1,22 +1,23 @@
 (RaiselyComponents) => class SimpleCard extends React.Component {
 	render() {
+		const { Link } = RaiselyComponents;
 		const card = this.props.getValues();
-     
-      
+
+		console.log(card.link)
 		return <article className={`card card--${card.colour}${card.link ? ' card--link' : ''} ${card.image && 'card--has-image'}`}>
-                    
-          {card.link && <a className="card__link" href={card.link}/>}
-          
+
+          {card.link && <Link className="card__link" href={card.link}/>}
+
           {card.title && <div className="card__title">
             <h4>{card.title}</h4>
             {card.link && <i className="material-icons">arrow_forward</i>}
           </div>}
-          
+
           {(card.image || card.text) && <div className="card__content">
             {card.image && <div className="card__content-image" style={{ backgroundImage: `url(${card.image}` }}/>}
             {card.text && <div className="card__content-text">{card.text}</div>}
           </div>}
-          
+
         </article>;
 	}
 }
