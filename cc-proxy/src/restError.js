b92e322f20672a44b5b809ba9b00e64040a03761
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2019-08-22 08:46:26 +0800

    Add all keys to RestError

--- cc-proxy/src/restError.js
+++ cc-proxy/src/restError.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 class RestError extends Error {
 	constructor(options) {
 		super(options.message);
-		Object.assign(this, _.pick(options, ['code', 'status']));
+		Object.assign(this, options);
 	}
 }
 
