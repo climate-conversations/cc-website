b832709de923715d492a7ef472b90c4c0c83cfcd
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2019-09-02 20:17:15 +0800

    Load private events

--- cc-raisely-components/components/view-conversation/view-conversation.js
+++ cc-raisely-components/components/view-conversation/view-conversation.js
@@ -2,19 +2,22 @@
 	const DisplayRecord = RaiselyComponents.import('display-record');
 	const GuestList = RaiselyComponents.import('conversation-guest-list');
 
+	const { Spinner } = RaiselyComponents;
 	const { Button } = RaiselyComponents.Atoms;
+	const { quickLoad } = RaiselyComponents.api;
 
 	const fields = ['event.date', 'event.name', 'event.address1', 'event.address2', 'event.postcode'];
 
 	return class ViewConversation extends React.Component {
+		state = { loading: true };
+
 		componentDidMount() {
 			this.load();
 		}
 
 		async load() {
-			this.setState({ loading: true });
 			try {
-				const data = await quickLoad({ models: ['event'], required: true, props: this.props });
+				const data = await quickLoad({ models: ['event.private'], required: true, props: this.props });
 				this.setState({ conversation: data.event });
 			} catch (e) {
 				this.setState({ error: e.message });
@@ -33,7 +36,7 @@
 			if (error) {
 				return (
 					<div className="view-conversation">
-						<div class="view-converasation-error">Error: {error}</div>
+						<div className="view-converasation-error">Error: {error}</div>
 					</div>
 				);
 			}
