ca47a027417151fedf6212f1ff06002276926a4f
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2019-09-13 13:32:59 +0800

    Fixes for view conversation

--- cc-raisely-components/components/view-conversation/view-conversation.js
+++ cc-raisely-components/components/view-conversation/view-conversation.js
@@ -1,12 +1,15 @@
+import { api } from "../../../raisely-modules/packages/raisely-api/src/api";
+
 (RaiselyComponents, React) => {
+	const { Spinner } = RaiselyComponents;
+	const { Button } = RaiselyComponents.Atoms;
+	const { quickLoad, getQuery, getData } = RaiselyComponents.api;
+	const { get } = RaiselyComponents.Common;
+
 	const DisplayRecord = RaiselyComponents.import('display-record');
 	const GuestList = RaiselyComponents.import('conversation-guest-list');
 
-	const { Spinner } = RaiselyComponents;
-	const { Button } = RaiselyComponents.Atoms;
-	const { quickLoad } = RaiselyComponents.api;
-
-	const fields = ['event.date', 'event.name', 'event.address1', 'event.address2', 'event.postcode'];
+	const fields = ['event.date', 'event.name', 'event.address1', 'event.address2'];
 
 	return class ViewConversation extends React.Component {
 		state = { loading: true };
@@ -15,10 +18,62 @@
 			this.load();
 		}
 
+		// eslint-disable-next-line class-methods-use-this
+		async getRsvps(eventUuid) {
+			const rsvps = await getData(api.eventRsvps.getAll({ query: { eventUuid, private: 1 } }));
+			return rsvps;
+		}
+
+		getFacilitator(rsvps) {
+			const facils = rsvps
+				.filter(({ type }) => ['facilitator', 'co-facilitator'].includes(type))
+				.map(rsvp => rsvp.user);
+
+			const userUuid = get(this.props, 'current.user.uuid');
+			const facilitator = facils.find(f => f.uuid === userUuid) || facils[0];
+			return facilitator;
+		}
+		// eslint-disable-next-line class-methods-use-this
+		getCounters(surveys) {
+			return {
+				hosts: surveys.filter(s => get(s, 'private.host') || get(s, 'private.hostCorporate')),
+				facilitators: surveys.filter(s => get(s, 'private.facilitate')),
+			};
+		}
+
 		async load() {
 			try {
-				const data = await quickLoad({ models: ['event.private'], required: true, props: this.props });
-				this.setState({ conversation: data.event });
+				const eventUuid = this.props.conversation ||
+					get(this.props, 'match.params.event') ||
+					getQuery(get(this.props, 'router.location.search')).event;
+
+
+				const promise = quickLoad({ models: ['event.private'], required: true, props: this.props })
+					.then(({ event: conversation }) => this.setState({ conversation }));
+
+				const rsvps = await this.getRsvps();
+				const facilitator = await this.getFacilitator(rsvps);
+
+				const [reflections, surveys] = await Promise.all([
+					getData(api.interactions.getAll({
+						query: {
+							category: 'facilitator-reflection', recordUuid: eventUuid,
+						},
+					})),
+					getData(api.interactions.getAll({
+						query: {
+							category: 'cc-post-survey-2019', recordUuid: eventUuid,
+						},
+					})),
+				]);
+				const guests = rsvps
+					.filter(r => r.type === 'guest');
+
+				const counters = this.getCounters(rsvps, surveys);
+				this.setState({ reflections, surveys, guests, counters });
+
+				// Catch exception
+				await promise;
 			} catch (e) {
 				this.setState({ error: e.message });
 				console.error(e);
@@ -28,7 +83,10 @@
 		}
 
 		render() {
-			const { conversation, loading, error } = this.state;
+			if (!this.state) {
+				return <Spinner />;
+			}
+			const { conversation, loading, error, counters, guests } = this.state;
 
 			if (loading) {
 				return <Spinner />;
@@ -44,13 +102,18 @@
 			return (
 				<div className="view-conversation">
 					<DisplayRecord event={conversation} fields={fields} />
+					<div className="view-conversation__stats">
+						<div className="view-conversation__stat">{guests.length} guests</div>
+						<div className="view-conversation__stat">{counters.hosts} new hosts</div>
+						<div className="view-conversation__stat">{counters.facilitators} new facilitators</div>
+					</div>
 					<div className="view-conversation__buttons">
 						<Button>Process Conversation</Button>
 						<Button>Facilitators Reflection</Button>
 						<Button>Reconcile Donations</Button>
 					</div>
 					<div className="view-conversation__guest-list">
-						<GuestList conversation={conversation} />
+						<GuestList conversation={conversation.uuid} />
 					</div>
 				</div>
 			);
