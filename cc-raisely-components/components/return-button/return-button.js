19e9e0e8da67be23d49a71eda43427d585ce9eb6
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2019-09-13 13:32:43 +0800

    Document return button

--- cc-raisely-components/components/return-button/return-button.js
+++ cc-raisely-components/components/return-button/return-button.js
@@ -1,3 +1,28 @@
+/**
+ * This component is a helper for chaining urls together
+ * use it's static helpers to generate a url with a return url encoded in the query
+ * Then either place this button on the page to return when done
+ * or use the static helper to extract the return url to push onto the
+ * history
+ *
+ * @example
+ * // At /conversations/list
+ * const bookUrl = ReturnButton.createReturningLink({
+ * 		props: this.props,
+ * 		url: '/conversations/create',
+ * 		done: 'booked'
+ * });
+ *
+ * // Then at /conversations/create
+ * render() {
+ * 		return <ReturnButton saveLabel="Save Booking" />
+ * }
+ * // Generates a button that when clicked goes to /conversations/list?done=booked
+ *
+ * // or to go there manually from within a function:
+ * const returnUrl = ReturnButton.getReturnUrl(this.props, '/defaultPage', true);
+ * this.props.location.history.push(returnUrl);
+ */
 (RaiselyComponents, React) => {
 	const { Button } = RaiselyComponents.Atoms;
 	const { getQuery } = RaiselyComponents.api;
@@ -20,6 +45,13 @@
 		return finalUrl;
 	}
 
+	/**
+	 * Get the url to visit in order to return to the previous page (optionally
+	 * indicate to the page that this page was completed)
+	 * @param {object} query query string of the current page decoded into an object
+	 * @param {string} defaultUrl URL if returnTo is not present on the query
+	 * @param {boolean} isDone Has this form been completed?
+	 */
 	function getReturnUrl(query, defaultUrl, isDone) {
 		const { onDone, returnTo } = query;
 		const returnUrl = returnTo || defaultUrl;
@@ -30,15 +62,55 @@
 	}
 
 	return class ReturnButton extends React.Component {
+		/**
+		 * Get the query from the props and determine the return to url
+		 * (optionally indicate to the page that this page was completed)
+		 * @param {object} props props (must include router.location.search)
+		 * @param {string} defaultUrl URL if returnTo is not present on the query
+		 * @param {boolean} isDone Has this form been completed?
+		 * @return {string} The return url extracted from the query
+		 */
 		static getReturnUrl(props, defaultUrl, isDone) {
 			const query = getQuery(get(props, 'router.location.search'));
 
 			return getReturnUrl(query, defaultUrl, isDone);
 		}
 
-		static createReturningLink({ props, url, done }) {
-			const { pathname, search } = get(props, 'router.location');
-			const thisPage = `${pathname}${search.startsWith('?') ? '' : '?'}${search}`;
+		/**
+		 * If you're at a url that received returnTo, but you want to go
+		 * somewhere else first and then return, this does the magic
+		 *
+		 * ie
+		 * firstUtr -> secondUrl?returnTo -> thirdUrl?returnTo -> firstUrl
+		 * use this if you are here ^
+		 * @param {object} props Include router.location.search
+		 * @param {string} url The url to add place returnTo and done params on
+		 * @return {string} The url with returnTo merged into the query
+		 */
+		static forwardReturnTo({ props, url }) {
+			const query = getQuery(get(props, 'router.location.search'));
+			const { onDone, returnTo } = query;
+			return this.createReturningLink({ returnTo, done: onDone, url });
+		}
+
+		/**
+		 * Generate a url that encodes the current url to in returnTo query
+		 * to return to a given page once the form on that page is done
+		 * If returnTo is not specified then the current page is used
+		 *
+		 * @param {object} options.props props (must include router.location)
+		 * @param {string} options.url The url to visit (and encode returnTo on)
+		 * @param {boolean} options.done Value to pass back to this page in the done query if
+		 * the page completes
+		 * @param {string} options.returnTo Page to return to after done (null for current page)
+		 * @return {string} The url to visit with returnTo on the query
+		 */
+		static createReturningLink({ props, url, done, returnTo }) {
+			let thisPage = returnTo;
+			if (!thisPage) {
+				const { pathname, search } = get(props, 'router.location');
+				thisPage = `${pathname}${search.startsWith('?') ? '' : '?'}${search}`;
+			}
 
 			return mergeQuery(url, {
 				returnTo: thisPage,
