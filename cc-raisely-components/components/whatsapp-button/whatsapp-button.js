72cc03bb4375d2d0f985ebf5f1d4f4ed7aaac69d
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2019-09-24 15:43:31 +0800

    Fix modular implementaiton

--- cc-raisely-components/components/whatsapp-button/whatsapp-button.js
+++ cc-raisely-components/components/whatsapp-button/whatsapp-button.js
@@ -1,25 +1,42 @@
-(RaiselyComponents, React) => class WhatsappButton extends React.Component {
-	static normalizePhoneNumber(phone) {
+(RaiselyComponents, React) => {
+	const { Icon } = RaiselyComponents.Atoms;
+
+	function normalizePhoneNumber(phone) {
 		let cleanPhone = phone.replace(/[^0-9]+/g, '');
 		// If it's 8 characters, prepend Singapore country code
 		if (cleanPhone.length === 8) cleanPhone = `65${cleanPhone}`;
 		return cleanPhone;
 	}
 
-	static generateUrl(phone, message) {
-		const urlMessage = encodeURIComponent(message);
-		const urlPhone = this.normalizePhone(phone);
-		const url = `https://api.whatsapp.com/send?phone=${urlPhone}&text=${urlMessage}`;
-		return url;
-	}
+	return class WhatsappButton extends React.Component {
+		static normalizePhoneNumber(phone) {
+			return normalizePhoneNumber(phone);
+		}
 
-	render() {
-		const { Button } = RaiselyComponents.Atoms;
+		static generateUrl(phone, message) {
+			const urlMessage = message ? encodeURIComponent(message) : '';
+			const urlPhone = phone ? normalizePhoneNumber(phone) : '';
+			const url = `https://api.whatsapp.com/send?phone=${urlPhone}&text=${urlMessage}`;
+			return url;
+		}
 
-		const { message, phone, label } = Object.assign({
-			label: 'Send WhatsApp',
-		}, this.props);
-		const url = this.constructor.generateUrl(phone, message);
-		return <Button href={url}>{label}</Button>;
-	}
+		render() {
+			const { Button } = RaiselyComponents.Atoms;
+
+			const { message, phone } = this.props;
+			let url;
+			let { label } = this.props;
+			if (typeof label !== 'string') label = <Icon name="perm_phone_msg" />;
+
+			if (phone) {
+				url = this.constructor.generateUrl(phone, message);
+			}
+			return (
+				<Button disabled={!phone} href={url} className="button--whatsapp">
+					{/* {label} */}
+					<Icon name="perm_phone_msg" size="small" />
+				</Button>
+			);
+		}
+	};
 };
