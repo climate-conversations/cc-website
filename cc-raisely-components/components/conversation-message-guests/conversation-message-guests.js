73d9744fe43135d688ef1b6002220e967534e914
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2019-09-24 15:42:45 +0800

    Rename email to message

---
+++ cc-raisely-components/components/conversation-message-guests/conversation-message-guests.js
@@ -0,0 +1,63 @@
+(RaiselyComponents, React) => {
+	const { get } = RaiselyComponents.Common;
+	const { api, Spinner } = RaiselyComponents;
+	const { getData, getQuery } = api;
+
+	const Messenger = RaiselyComponents.import('message-send-and-save');
+	const ReturnButton = RaiselyComponents.import('return-button');
+	const ConversationRef = RaiselyComponents.import('conversation', { asRaw: true });
+
+	let Conversation;
+
+	return class EmailConversationGuest extends React.Component {
+		state = { loading: true };
+
+		componentDidMount() {
+			this.load();
+		}
+		load = async () => {
+			if (!Conversation) Conversation = ConversationRef().html;
+			const { props } = this;
+			const results = await Conversation.loadRsvps({ props, type: ['guest'] });
+
+			this.setState({ ...results, loading: false });
+		}
+
+		renderInner() {
+			const { loading, guests } = this.state;
+			if (loading) {
+				return <Spinner />;
+			}
+
+			if (!guests.length) {
+				return (
+					<div className="error">There are no contactable guests in this conversation</div>
+				);
+			}
+
+			const defaultMessage = 'Thank you for attending the Climate Conversation!';
+			const body = get(this.props, 'global.campaign.public.conversationGuestThankyou', defaultMessage);
+
+			const params = {
+				...this.props,
+				sendby: 'email',
+				body,
+				to: guests,
+				subject: "Thank you & what's next?",
+			};
+
+			return <Messenger {...params} />;
+		}
+
+		render() {
+			const { props } = this;
+			const { error } = this.state;
+			return (
+				<div className="email--guests__wrapper">
+					{error ? <div className="error">{error}</div> : this.renderInner()}
+					<ReturnButton {...props} backLabel="Go Back" backTheme="secondary" />
+				</div>
+			)
+		}
+	};
+};
