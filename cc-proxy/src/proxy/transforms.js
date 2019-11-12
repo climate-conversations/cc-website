3312b3d9308b20a0c0e7e5ede9c0d0623d82afc0
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2019-11-13 07:51:44 +0800

    Document transforms

--- cc-proxy/src/proxy/transforms.js
+++ cc-proxy/src/proxy/transforms.js
@@ -1,5 +1,14 @@
 const { pick } = require('lodash');
 
+/**
+ * These functions transform the response from the proxy
+ */
+
+/**
+ * Return only uuid and preferredName from a request that returns a user
+ * @param {object} body Response body
+ * @return {object} Transformed body
+ */
 function minimalUser(body) {
 	const { data } = body;
 
