9dc9fafe91da529ea4a36cde4d2ba53c531674ab
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2019-12-10 16:05:01 +0800

    Remove keys from server

--- conversation-sync/server.js
+++ conversation-sync/server.js
@@ -1,8 +1,6 @@
 require('./config');
 const routes = require('./config/routes');
 
-Object.keys(process.env).forEach(key => console.log(`${key} ${process.env[key]}`));
-
 module.exports = {};
 
 routes.forEach((route) => { module.exports[route.path] = route.fn; });
