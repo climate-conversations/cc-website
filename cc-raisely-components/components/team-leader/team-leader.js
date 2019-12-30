27cab83a5b014a1a81ee9acfb99c07c6609d64b2
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2019-12-30 11:10:23 +1100

    Add component for displaying team contact info

---
+++ cc-raisely-components/components/team-leader/team-leader.js
@@ -0,0 +1,82 @@
+(RaiselyComponents, React) => {
+	const { api, Common } = RaiselyComponents;
+	const { get } = Common;
+	const UserSaveHelperRef = RaiselyComponents.import('cc-user-save');
+	const WhatsAppButton = RaiselyComponents.import('whatsapp-button');
+	let UserSaveHelper;
+
+	return class TeamContact extends React.Component {
+		state = { loading: true };
+		async load() {
+			try {
+				const { profile } = await api.quickLoad({ props: this.props, models: ['profile.private'], required: true });
+				this.setState({ profile, facilitator: profile.user });
+
+				if (!UserSaveHelper) UserSaveHelper = UserSaveHelperRef().html;
+				const { teamProfile } = await UserSaveHelper.proxy(`/profiles/${profile.parentUuid}?private=1`, {
+					method: 'get',
+				});
+				this.setState({ teamProfile, teamLeader: teamProfile.user });
+			} catch (e) {
+				console.error(e);
+				this.setState({ error: e.message, loading: false });
+			}
+		}
+
+		nouns(facilitator) {
+			const currentUuid = get(this.props, 'current.user.uuid', 'logged-out');
+			if (currentUuid === get(facilitator, 'uuid', 'n/a')) {
+				return {
+					you: 'You',
+					your: 'Your',
+				}
+			}
+			const noun = get(facilitator, 'preferredName') || get(facilitator, 'fullName');
+			return {
+				you: noun,
+				your: `${noun}'s`,
+			};
+		}
+
+		renderForTeam() {
+			const { teamProfile } = this.state;
+			return (
+				<div className="team-membership-details">
+					<div className="team-membership__team">
+						<WhatsAppButton
+							label="Open Group Chat"
+							link={get(teamProfile.private.teamChatUrl)}
+						/>
+						Note: This will add you to the group chat if you are not already a member
+					</div>
+				</div>
+			)
+		}
+
+		render() {
+			const { teamProfile, facilitator, teamLeader } = this.state;
+			const { you, your } = this.nouns(facilitator);
+			const leaderName = get(teamLeader, 'preferredName') || get(teamLeader, 'fullName');
+			return (
+				<div className="team-membership-details">
+					<div className="team-membership__team">
+						<span className="team-membership__team-name">{you} are part of</span>
+						<h4>{teamProfile.name}</h4>
+						<WhatsAppButton
+							label="Open Group Chat"
+							link={get(teamProfile.private.teamChatUrl)}
+						/>
+					</div>
+					{your} team leader is {leaderName}
+					<div className="team-membership__leader-contact">
+						<Icon
+						/>
+						<WhatsAppButton
+							phone={get(teamLeader, 'phoneNumber')}
+						/>
+					</div>
+				</div>
+			);
+		}
+	};
+};
