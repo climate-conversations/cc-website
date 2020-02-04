7135087187d0a94389e58d9451af08a321e822a5
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-02-04 11:52:35 +0800

    Fix styling

--- cc-raisely-components/components/raisely-button/raisely-button.js
+++ cc-raisely-components/components/raisely-button/raisely-button.js
@@ -19,7 +19,7 @@
 		const width = sizeMap[size] || sizeMap.small;
 
 		return (
-			<svg width={width} height={width} xmlns="http://www.w3.org/2000/svg">
+			<svg className="raisely--button__logo" width={width} height={width} xmlns="http://www.w3.org/2000/svg">
 				<path d="M22.548 20.18l-9.047-10.172-9.059 10.158A11.19 11.19 0 0 1 2.25 13.5C2.25 7.296 7.296 2.25 13.5 2.25S24.75 7.295 24.75 13.5c0 2.499-.819 4.81-2.202 6.68M13.5 24.75a11.207 11.207 0 0 1-7.54-2.907L13.5 13.39l7.528 8.464a11.206 11.206 0 0 1-7.527 2.897M0 13.5C0 20.956 6.044 27 13.5 27S27 20.956 27 13.5 20.956 0 13.5 0 0 6.044 0 13.5z" fill="currentColor" fillRule="evenodd" />
 			</svg>
 		);
@@ -55,7 +55,7 @@
 		}
 		const Element = label ? Button : Link;
 		return (
-			<Element href={url} target="raisely" size={size}>
+			<Element className="raisely--button" href={url} target="raisely" size={size}>
 				<RaiselyLogo size={size} />
 				{label || ''}
 			</Element>
