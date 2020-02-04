e522e43c60d7194dd8b14f67b32f20e36f10100d
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-02-04 11:55:15 +0800

    Fixes to data entry

--- cc-raisely-components/components/guest-data-entry/guest-data-entry.js
+++ cc-raisely-components/components/guest-data-entry/guest-data-entry.js
@@ -7,11 +7,9 @@
 
 	const CustomForm = RaiselyComponents.import('custom-form');
 	const UserSaveHelperRef = RaiselyComponents.import('cc-user-save', { asRaw: true });
+	const ConversationRef = RaiselyComponents.import('conversation', { asRaw: true });
 	let UserSaveHelper;
-
-	const preSurvey = 'cc-pre-survey-2019';
-	const postSurvey = 'cc-post-survey-2019';
-	const surveys = [preSurvey, postSurvey];
+	let Conversation;
 
 	let actionFields;
 
@@ -21,59 +19,77 @@
 	 */
 	const actionFieldsToUser = ['host', 'facilitate', 'volunteer'];
 
-	// Show all questions in the pre survey
-	const preSurveyQuestions = [{ interactionCategory: preSurvey }];
-	// Show all questions in the post survey
-	const postSurveyQuestions = [{
-		interactionCategory: postSurvey,
-		exclude: ['research', 'fundraise', 'host', 'volunteer', 'corporate', 'facilitate'],
-	}];
-
-	const guestAction = [
-		'user.fullName',
-		{ sourceFieldId: 'user.email', required: false },
-		'user.phoneNumber', 'user.postcode',
-		{
-			interactionCategory: postSurvey,
-			include: ['host', 'facilitate', 'volunteer', 'corporate', 'research', 'fundraise'],
-		},
-	];
 	const guestDonation = ['event_rsvp.donationIntention', 'event_rsvp.donationAmount'];
 	const conversationDate = [{ sourceFieldId: 'event.startAt', required: false }];
 
-	const allSteps = [
-		{ title: 'Pre-Survey', fields: preSurveyQuestions },
-		{ title: 'Post-Survey', fields: postSurveyQuestions },
-		{ title: 'Guest Action', fields: guestAction },
-		{
-			title: 'Warning: Uncontactable',
-			description: `This person has indicated interest in taking action with us
-				but they have no email address. Follow up will be very limited.`,
-			fields: [{
-				id: 'description-no-email',
-				type: 'rich-description',
-				default: `
-					<p>Do you want to go back and enter an email for them?</p>
-					<p>(Maybe you can message the host to ask
-						if they want to share their email with us?)</p>`,
-			}],
-			condition: (fields) => {
-				const takeAction = actionFields.reduce((result, field) =>
-					result || get(fields, `2.private.${field}`, false), false);
-				return (takeAction && !get(fields, '2.email'));
-			},
-		},
-		{ title: 'Donation', fields: guestDonation },
-		{
-			title: 'Conversation Date',
-			description: "Did you set a tentative date with them to host a conversation? (leave blank if you didn't)",
-			fields: conversationDate,
-			condition: fields => get(fields, '2.private.host'),
-		},
-	];
-
 	return class GuestDataEntry extends React.Component {
-		state = { key: new Date().toISOString() }
+		state = { key: new Date().toISOString() };
+
+		prepareSteps() {
+			if (!Conversation) Conversation = ConversationRef().html;
+
+			// Show all questions in the pre survey
+			const preSurveyQuestions = [
+				'user.nycConsent',
+				{ interactionCategory: Conversation.surveyCategories().preSurvey },
+				'user.dateOfBirth',
+				{
+					sourceFieldId: 'user.residency',
+					type: 'select',
+					options: [{"label": "Singapore Citizen", "value": "citizen"}, {"label": "Permanent Resident", "value": "permanent resident"}, {"label": "Employment Pass", "value": "employment pass"},{"label": "Other", "value": "Other"}],
+				},
+				'user.ethnicity',
+				'user.gender',
+			];
+			// Show all questions in the post survey
+			const postSurveyQuestions = [{
+				interactionCategory: Conversation.surveyCategories().postSurvey,
+				exclude: ['research', 'fundraise', 'host', 'volunteer', 'hostCorporate', 'facilitate'],
+			}];
+
+			const guestAction = [
+				'user.fullName',
+				'user.preferredName',
+				{ sourceFieldId: 'user.email', required: false },
+				'user.phoneNumber', 'user.postcode',
+				{
+					interactionCategory: Conversation.surveyCategories().postSurvey,
+					include: ['host', 'facilitate', 'volunteer', 'hostCorporate', 'research', 'fundraise'],
+				},
+			];
+
+			const allSteps = [
+				{ title: 'Pre-Survey', fields: preSurveyQuestions },
+				{ title: 'Post-Survey', fields: postSurveyQuestions },
+				{ title: 'Guest Action', fields: guestAction },
+				{
+					title: 'Warning: Uncontactable',
+					description: `This person has indicated interest in taking action with us
+						but they have no email address. Follow up will be very limited.`,
+					fields: [{
+						id: 'description-no-email',
+						type: 'rich-description',
+						default: `
+							<p>Do you want to go back and enter an email for them?</p>
+							<p>(Maybe you can message the host to ask
+								if they want to share their email with us?)</p>`,
+					}],
+					condition: (fields) => {
+						const takeAction = actionFields.reduce((result, field) =>
+							result || get(fields, `2.private.${field}`, false), false);
+						return (takeAction && !get(fields, '2.email'));
+					},
+				},
+				{ title: 'Donation', fields: guestDonation },
+				{
+					title: 'Conversation Date',
+					description: "Did you set a tentative date with them to host a conversation? (leave blank if you didn't)",
+					fields: conversationDate,
+					condition: fields => get(fields, '2.private.host'),
+				},
+			];
+			return allSteps;
+		}
 
 		load = async () => {
 			const data = await quickLoad({
@@ -90,13 +106,19 @@
 			const facilitatorUuid = this.props.global.user.uuid;
 			const campaignUuid = this.props.global.campaign.uuid;
 
+			if (!Conversation) Conversation = ConversationRef().html;
+			const surveyCategories = Conversation.surveyCategories();
+			const surveys = [surveyCategories.preSurvey, surveyCategories.postSurvey];
+
 			// Copy interaction host, vol, facil over to user record
 			actionFieldsToUser.forEach((field) => {
-				const value = get(data, `interaction.${postSurvey}.detail.private.${field}`);
+				const value = get(data, `interaction.${surveyCategories.postSurvey}.detail.private.${field}`);
 				if (value) {
 					set(data, `user.private.${field}`, value);
 				}
 			});
+			// Mark them having attended a conversation
+			set(data, 'user.private.attendedConversation', true);
 
 			console.log('Saving survey. Form data:', data);
 
@@ -129,7 +151,7 @@
 				.map(record => api.interactions.create({ data: record }))
 				.map(getData);
 
-			if (get(data, `interaction.${postSurvey}.detail.private.host`)) {
+			if (get(data, `interaction.${surveyCategories.postSurvey}.detail.private.host`)) {
 				promises.push(getData(api.interactions.create({
 					data: {
 						...interactionBase,
@@ -154,15 +176,20 @@
 				eventUuid: this.event.uuid,
 			});
 
-			promises.push(getData(api.eventRsvps.create({ data: data.event_rsvp })));
+			promises.push(
+				getData(api.eventRsvps.create({ data: data.event_rsvp }))
+					.then(rsvp => this.setState({ rsvpUuid: rsvp.uuid }))
+			);
 
 			// Save donation / donation intention
-			if (get(data, 'event_rsvp.private.donationIntention') === 'cash') {
+			const cashPaymentType = get(data, 'event_rsvp.private.donationIntention');
+			if (['transfer', 'cash'].includes(cashPaymentType)) {
 				promises.push(getData(api.donations.create({
 					data: {
 						campaignUuid,
 						profileUuid: this.props.global.campaign.profile.uuid,
 						userUuid: user.uuid,
+						anonymous: true,
 						mode: 'LIVE',
 						type: 'OFFLINE',
 						method: 'OFFLINE',
@@ -170,6 +197,7 @@
 						email: user.email,
 						private: {
 							conversationUuid: this.event.uuid,
+							cashPaymentType,
 						},
 						currency: 'SGD',
 					},
@@ -205,7 +233,24 @@
 				promises.push(promise);
 			}
 
-			return Promise.all(promises);
+			await Promise.all(promises);
+
+			// Send the guest to be added to the backend spreadsheet
+			const webhookUrl = `https://asia-northeast1-climate-conversations-sync.cloudfunctions.net/raiselyPeople`;
+			await UserSaveHelper.doFetch(webhookUrl, {
+				method: 'post',
+				body: {
+					data: {
+						user,
+						preSurvey: get(data, `interaction.${surveyCategories.preSurvey}.detail`),
+						postSurvey: get(data, `interaction.${surveyCategories.postSurvey}.detail`),
+						conversation: this.event,
+						rsvp: get(data, 'event_rsvp'),
+					}
+				}
+			});
+
+			throw new Error('Successful');
 		}
 
 		saving() {
@@ -225,7 +270,7 @@
 				details = this.state.saving.map((setting) => {
 					if (setting.message) hasError = true;
 					const status = setting.message || 'OK';
-					return <p>Saving {descriptions[setting.id]} ... {status}</p>;
+					return <p key={setting}>Saving {descriptions[setting.id]} ... {status}</p>;
 				});
 			}
 
@@ -233,12 +278,22 @@
 		}
 
 		finalPage = ({ completeHref }) => {
+			const { rsvpUuid } = this.state;
 			// Changing the key will cause react to re-fresh the component
 			const reset = () => this.setState({ key: new Date().toISOString() });
+			const reviewLink =  `/surveys/${rsvpUuid}`;
 
 			return (
 				<div>
 					<p>Guest saved!</p>
+					<div>
+						<p>
+							As this is the first conversation you've processed in the new system,
+							could you do us a favour and review the saved details and check
+							that everything was saved correctly?
+						</p>
+						<Button theme="cta" href={reviewLink}>Review Guest Details</Button>
+					</div>
 					<div>
 						<Button theme="primary" onClick={reset}>Add another Guest</Button>
 						<Button theme="secondary" href={completeHref}>Go back to dashboard</Button>
@@ -249,19 +304,24 @@
 
 		render() {
 			if (!UserSaveHelper) {
-				try {
-					UserSaveHelper = UserSaveHelperRef().html;
-					({ actionFields } = UserSaveHelper);
-				} catch (e) {}
+				UserSaveHelper = UserSaveHelperRef().html;
+				({ actionFields } = UserSaveHelper);
 			}
 
 			const { key } = this.state;
 
 			return (
 				<div className="guest-data-entry-wrapper" key={key}>
+					<div className="hint">
+						<p><strong>Hint:</strong> To get throught your surveys faster, press <strong>Tab</strong> between fields and type in the answers.</p>
+						<p>For scale fields, type in the number that was ticked.</p>
+						<p>For multiple choice fields, press the down arrow or type the first few characters of the answer you want to choose</p>
+						<p>For checkboxes press spacebar to check/uncheck the box</p>
+						<p>(You can do this on all forms in the volunteer portal, not just this one)</p>
+					</div>
 					<CustomForm
 						{...this.props}
-						steps={allSteps}
+						steps={this.prepareSteps()}
 						followNextQuery="1"
 						controller={this}
 					/>
