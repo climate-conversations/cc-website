ba566288f41696fca7ceb43368b17245bb5dee79
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-02-04 11:56:38 +0800

    Major changes to user save

--- cc-raisely-components/components/cc-user-save/cc-user-save.js
+++ cc-raisely-components/components/cc-user-save/cc-user-save.js
@@ -4,66 +4,66 @@
 	const proxyUrl = 'https://asia-northeast1-climate-conversations-sg-2019.cloudfunctions.net/proxy';
 	const upsertUrl = 'https://asia-northeast1-climate-conversations-sg-2019.cloudfunctions.net/upsertUser';
 
-	async function doFetch(url, options) {
-		let json;
-		// Message stores the best error message if an exception is
-		// thrown along the way
-		let message = 'Cannot reach server (are you offline?)';
-
-		const opts = Object.assign({
-			mode: 'cors',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		}, options);
-
-		try {
-			// Send the users token for authorization
-			const token = getCurrentToken();
-			if (token) opts.headers.Authorization = `Bearer ${token.replace(/"/g, '')}`;
-
-			console.log('Doing fetch', url, opts);
-			if (opts.body && typeof opts.body !== 'string') {
-				opts.body = JSON.stringify(opts.body);
-			}
-			const response = await fetch(url, opts);
-
-			// If the request didn't succeed, log the error
-			// and try to create a helpful error for the user
-			if (response.status !== 200) {
-				// Default to showing the response text
-				message = `Error: ${response.statusText}`;
-
-				// But try to get a better message if we can
-				json = await response.json();
-				console.error(json);
-				// eslint-disable-next-line prefer-destructuring
-				message = json.errors[0].message;
-
-				throw new Error(message);
-			}
-
-			// If the request succeeded with no JSON, that's weird
-			message = 'Sorry, something unusual went wrong';
-			json = await response.json();
-		} catch (error) {
-			// If fetch has thrown an error the message is pretty cryptic
-			// log the message for developers and give the end user a better message
-			console.error(error);
-			error.oldMessage = error.message;
-			error.message = message;
-			throw error;
-		}
-
-		return json.data;
-	}
-
 	return class UserSaveHelper {
-		static actionFields = ['host', 'facilitate', 'volunteer', 'corporate', 'research', 'fundraise'];
+		static actionFields = ['host', 'facilitate', 'volunteer', 'hostCorporate', 'research', 'fundraise'];
+
+		static async doFetch(url, options) {
+			let json;
+			// Message stores the best error message if an exception is
+			// thrown along the way
+			let message = 'Cannot reach server (are you offline?)';
+
+			const opts = Object.assign({
+				mode: 'cors',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}, options);
+
+			try {
+				// Send the users token for authorization
+				const token = getCurrentToken();
+				if (token) opts.headers.Authorization = `Bearer ${token.replace(/"/g, '')}`;
+
+				console.log('Doing fetch', url, opts);
+				if (opts.body && typeof opts.body !== 'string') {
+					opts.body = JSON.stringify(opts.body);
+				}
+				const response = await fetch(url, opts);
+
+				// If the request didn't succeed, log the error
+				// and try to create a helpful error for the user
+				if (response.status !== 200) {
+					// Default to showing the response text
+					message = `Error: ${response.statusText}`;
+
+					// But try to get a better message if we can
+					json = await response.json();
+					console.error(json);
+					// eslint-disable-next-line prefer-destructuring
+					message = json.errors[0].message;
+
+					throw new Error(message);
+				}
+
+				// If the request succeeded with no JSON, that's weird
+				message = 'Sorry, something unusual went wrong';
+				json = await response.json();
+			} catch (error) {
+				// If fetch has thrown an error the message is pretty cryptic
+				// log the message for developers and give the end user a better message
+				console.error(error);
+				error.oldMessage = error.message;
+				error.message = message;
+				throw error;
+			}
+
+			return json.data;
+		}
 
 		static async proxy(path, options) {
 			const url = `${proxyUrl}${path}`;
-			return doFetch(url, options);
+			return this.doFetch(url, options);
 		}
 
 		/**
@@ -71,7 +71,7 @@
 		 * @param {object} record
 		 */
 		static async upsertUser(record) {
-			return doFetch(upsertUrl, {
+			return this.doFetch(upsertUrl, {
 				method: 'post',
 				body: { data: record },
 			});
