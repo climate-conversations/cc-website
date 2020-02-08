6a9d67257a087136a62824e3c4798f18c9074991
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-02-08 14:08:43 +0800

    Fix conversation reconciliation

--- cc-raisely-components/components/conversation-reconcile/conversation-reconcile.js
+++ cc-raisely-components/components/conversation-reconcile/conversation-reconcile.js
@@ -1,6 +1,6 @@
 (RaiselyComponents, React) => {
 	// eslint-disable-next-line object-curly-newline
-	const { displayCurrency, get, kebabCase, momentjs } = RaiselyComponents.Common;
+	const { dayjs, displayCurrency, get, kebabCase, set } = RaiselyComponents.Common;
 	const { getData, save } = RaiselyComponents.api;
 	const { Button } = RaiselyComponents.Atoms;
 	const { Spinner } = RaiselyComponents;
@@ -40,11 +40,10 @@
 			try {
 				await Promise.all([
 					Conversation.loadConversation({ props, private: 1 })
-						.then(values => this.setState(values)),
+						.then(conversation => this.setState({ conversation, loading: false })),
 					Conversation.loadRsvps({ props, type: ['facilitator', 'guest'] })
 						.then(values => this.setState(values)),
 				]);
-				this.setState({ loading: false });
 			} catch (e) {
 				console.error(e);
 				this.setState({ loading: false, error: e.message });
@@ -56,7 +55,7 @@
 			let canShow;
 			do {
 				step += 1;
-				canShow = !steps[step].show || steps[step](this.state);
+				canShow = !steps[step].show || steps[step].show(this.state);
 			} while ((!canShow) && (step < (steps.length - 1)));
 			this.setState({ step });
 		}
@@ -64,7 +63,7 @@
 		imageChecked = (match) => {
 			const failed = this.state.failed || [];
 			const stepId = steps[this.state.step].id;
-			if (!match) this.state.failed.push(stepId);
+			if (!match) failed.push(stepId);
 			this.setState({ failed }, this.next);
 		}
 
@@ -78,7 +77,7 @@
 
 			return (
 				<div className={`reconcile--${className}`}>
-					<img href={url} alt="" />
+					<img src={url} alt={imageField} />
 					<p>Is the amount shown in the image {value}?</p>
 					<Button onClick={() => this.imageChecked(false)}>No</Button>
 					<Button onClick={() => this.imageChecked(true)}>Yes</Button>
@@ -95,29 +94,29 @@
 		 */
 		facilitatorMessage({ amounts, inconsistent }) {
 			const { failed, conversation } = this.state;
-			const date = momentjs(conversation.startAt).format('MMM dd');
+			const date = dayjs(conversation.startAt).format('MMM DD');
 			const url = `https://p.climate.sg/conversations/${conversation.uuid}/donations-report`;
 			const ftlName = get(this.props, 'global.user.preferredName');
 
-			const open = `Hi,
-			Can you help me to check the donations for the conversation on ${date} (${conversation.name})?`;
+			const open = `Hi, Can you help me to check the donations for the conversation on ${date} (${conversation.name})?`;
 
 			const messages = {
-				donationReport: `The amount on the donations report is not the same as the amount you wrote (${amounts.cashReceivedAmount})`,
-				transfer: `The amount on the transaction screenshot is not the same as the amount you wrote (${amounts.cashTransferAmount})`,
+				donationReport: `amount on the donations report is not the same as the amount you wrote (${amounts.cashReceivedAmount})`,
+				transfer: `amount on the transaction screenshot is not the same as the amount you wrote (${amounts.cashTransferAmount})`,
 			};
-			const imageErrors = ['donationReport', 'transfer']
+			let imageErrors = ['donationReport', 'transfer']
 				.map(key => (failed.includes(key) ? messages[key] : ''))
 				.filter(m => m)
-				.join(', and');
+				.join(', and the ');
+			if (imageErrors) imageErrors = `The ${imageErrors}`;
 
 			const allErrors = [
 				imageErrors,
 				inconsistent ?
 					`The cash amounts aren't all the same:
-					${amounts.cashReceivedAmount} on the donations report,
-					${amounts.cashTransferAmount} on the bank transfer,
-					${amounts.cashCtaAmount} according to CTA forms` : '',
+${amounts.cashReceivedAmount} on the donations report,
+${amounts.cashTransferAmount} on the bank transfer,
+${amounts.cashCtaAmount} according to CTA forms` : '',
 			]
 				.filter(m => m)
 				.join('. Also, ');
@@ -140,10 +139,12 @@ ${close}`;
 			console.log('Saving');
 			const data = formToData(values);
 			const { event } = data;
+			set(event, 'private.cashDonationsReviewerUuid', get(this.props.global.user.uuid));
 			return getData(save('event', event, { partial: true }));
 		}
 
-		reconcileStep({ amounts, inconsistent }) {
+		reconcileStep({ amounts, consistent }) {
+			const inconsistent = !consistent;
 			const facilMessage = this.facilitatorMessage({ amounts, inconsistent });
 			const facilNotes = get(this, 'state.conversation.private.cashDonationsFacilitatorNotes');
 			const { facilitators } = this.state;
@@ -153,10 +154,10 @@ ${close}`;
 				<div className="reconcile--notes">
 					{inconsistent ? (
 						<React.Fragment>
-							<p>The amounts are inconsistent, can you please check them with the faciltiator?</p>
+							<p>The donation amounts are inconsistent, can you please check them with the faciltiator?</p>
 							<div className="reconcile--amounts">
 								<ul>
-									<li>Amount in CTA forms: {amounts.cashCtaAmount}</li>
+									<li>Sum of amounts in CTA forms: {amounts.cashCtaAmount}</li>
 									<li>Amount written in donation report: {amounts.cashReceivedAmount}</li>
 									<li>Amount transferred by facilitator: {amounts.cashTransferAmount}</li>
 								</ul>
@@ -166,20 +167,24 @@ ${close}`;
 					<div className="reconcile--notes_facil-notes">
 						<p>
 							The facilitator has added the following notes about the donations received.
-							If these notes satisfactorily account for the donation amounts,
-							please tick the box below.
 						</p>
 						<div className="reconcile--facil-notes">{facilNotes}</div>
 					</div>
 					<p>
+						<strong>
+							If these notes satisfactorily account for the donation amounts,
+							please tick the box below.
+						</strong>
+					</p>
+					<p>
 						If you require more information from the facilitator,
 						click this button to follow up with them
+						<WhatsappButton
+							message={facilMessage}
+							phone={phone}
+							label="Whatsapp Facilitator"
+						/>
 					</p>
-					<WhatsappButton
-						message={facilMessage}
-						phone={phone}
-						label="Whatsapp Facilitator"
-					/>
 					<CustomForm
 						{...this.props}
 						steps={formSteps}
@@ -208,9 +213,10 @@ ${close}`;
 			amountFields.forEach((key) => {
 				amounts[key] = displayCurrency(get(this.state, `conversation.private.${key}`, 0), 'SGD');
 			});
-			const inconsistent = amountFields.reduce((last, key) => {
+			console.log(amounts);
+			const consistent = amountFields.reduce((last, key) => {
 				const current = amounts[key];
-				if (last === true || last === current) return current;
+				if (last === true || last === current) return current || true;
 				return false;
 			}, true);
 
@@ -219,17 +225,17 @@ ${close}`;
 					<div className="description">
 						Thank you for taking the time to review the donations and ensure
 						we are keeping good records.
-						{"Here's"} what you need to do:
+						{" Here's"} what you need to do:
 						<ol>
 							<li>Confirm that the amounts in the pictures match the amounts entered</li>
-							{inconsistent ? (
+							{!consistent ? (
 								<li>Check if {"there's"} a valid reason why the donation amounts {"don't"} match</li>
 							) : ''}
 						</ol>
 					</div>
 					{step < 2 ?
 						this.pictureConfirmationStep() :
-						this.reconcileStep({ amounts, inconsistent })}
+						this.reconcileStep({ amounts, consistent })}
 					<ReturnButton {...props} backLabel="Go back" />
 				</div>
 			);
