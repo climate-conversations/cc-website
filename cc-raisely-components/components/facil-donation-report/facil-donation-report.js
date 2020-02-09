d41f35980437b4fb6c7d79e2da45b068908f96c8
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-02-09 12:28:07 +0800

    Fixes and updates for reconciliation

--- cc-raisely-components/components/facil-donation-report/facil-donation-report.js
+++ cc-raisely-components/components/facil-donation-report/facil-donation-report.js
@@ -6,6 +6,10 @@
 	const { getData, save } = api;
 	const { get } = RaiselyComponents.Common;
 
+	const showTransferPage = values =>
+		get(values, '0.private.cashReceived') ||
+		get(values, '0.private.cashReceivedAmount');
+
 	return class FacilDonationReport extends React.Component {
 		generateForm() {
 			const cashReport = ['event.cashReceived', 'event.cashReceivedAmount', {
@@ -22,7 +26,15 @@
 
 			const multiFormConfig = [
 				{ title: 'Donation Report', fields: cashReport },
-				{ title: 'Transfer Cash', fields: transferReport, condition: values => get(values, '0.private.cashReceived') },
+				{ title: 'Transfer Cash', fields: transferReport, condition: showTransferPage },
+				{ title: 'Notes on Donations', fields: [
+					{
+						id: 'notes-description',
+						type: 'rich-description',
+						default: 'If you have anything you need to note about the cash donations, place those notes here',
+					},
+					'event.cashDonationsFacilitatorNotes'
+				]},
 			];
 
 			return multiFormConfig;
@@ -30,6 +42,7 @@
 
 		load = async ({ dataToForm }) => {
 			const records = await api.quickLoad({ props: this.props, required: true, models: ['event.private'] });
+			this.setState(records);
 			return dataToForm(records);
 		}
 
@@ -40,17 +53,31 @@
 			return getData(save('event', event, { partial: true }));
 		}
 
+		renderLeaderNotes() {
+			const leaderNotes = get(this.state, 'event.private.cashDonationsLeaderNotes')
+			if (!leaderNotes) return '';
+			return (
+				<div className="facil-donations-report--leader-notes--wrapper">
+					<div className="">Your facilitator left a note regarding your donations report</div>
+					<div className="team-leader-notes">{leaderNotes}</div>
+				</div>
+			);
+		}
+
 		render() {
 			const config = this.generateForm();
 			const description = 'As part of our governance requirements, we must keep good records of all donations received at conversations to demonstrate to our donors, grantors and auditors that we are trustworthy. Please complete this form in full.';
 			return (
-				<CustomForm
-					{...this.props}
-					steps={config}
-					controller={this}
-					description={description}
-					redirectToReturnTo="true"
-				/>
+				<div className="facil-donations-report--wrapper">
+					{this.renderLeaderNotes()}
+					<CustomForm
+						{...this.props}
+						steps={config}
+						controller={this}
+						description={description}
+						redirectToReturnTo="true"
+					/>
+				</div>
 			);
 		}
 	};
