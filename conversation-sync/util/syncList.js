716cee1520a01a3358221626dade9b4ddadbefd0
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-02-24 09:29:52 +0800

    Run sync in parallel

--- conversation-sync/util/syncList.js
+++ conversation-sync/util/syncList.js
@@ -1,7 +1,11 @@
+const _ = require('lodash');
+
 require('../config');
 
 const MailchimpController = require('../src/controllers/Mailchimp');
 const { raiselyRequest } = require('../src/helpers/raiselyHelpers');
+
+const CONCURRENCY=10;
 
 async function syncRaiselyToMailchimp() {
 	const controller = new MailchimpController({
@@ -12,12 +16,16 @@ async function syncRaiselyToMailchimp() {
 	let offset = parseInt(process.env.OFFSET) || 0;
 	let next;
 
+	const promises = [];
+	let error;
+
 	// Fetch raisely users
 	do {
 		console.log('Fetching records from raisely ...');
 		const body = await raiselyRequest({
 			path: next || `/users?private=1&limit=${limit}&offset=${offset}`,
 			fullResult: true,
+			token: process.env.RAISELY_TOKEN,
 		});
 
 		next = body.pagination.nextUrl;
@@ -27,17 +35,26 @@ async function syncRaiselyToMailchimp() {
 		for (let i = 0; i < users.length; i++) {
 			const user = users[i];
 
-
 			const data = {
 				type: 'user.updated',
 				data: user,
 			}
 			console.log(`**** Syncing person ${i + offset} ${user.uuid} ${user.email} ****`);
-			await controller.process({ data });
+			const promise = controller.process({ data }).catch(err => error = err);
+			promises.push(promise);
+			promise.then(() => {
+				_.remove(promises, x => x === promise);
+			});
+
+			if (promises.length > CONCURRENCY) await Promise.race(promises);
+			if (error) throw error;
 		}
 		offset += limit;
 
+		if (error) throw error;
+
 	} while (next);
+	if (error) throw error;
 }
 
 syncRaiselyToMailchimp()
