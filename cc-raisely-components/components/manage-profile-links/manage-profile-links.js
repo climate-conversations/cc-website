a0070dca5a29bcde0519a132f969bda395f0fb85
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-03-08 11:06:21 +0800

    Add admin support for links

--- cc-raisely-components/components/manage-profile-links/manage-profile-links.js
+++ cc-raisely-components/components/manage-profile-links/manage-profile-links.js
@@ -1,7 +1,10 @@
 (RaiselyComponents, React) => {
+	const { api } = RaiselyComponents;
+	const { getData } = api;
 	const { Button } = RaiselyComponents.Atoms;
+	const { get } = RaiselyComponents.Common;
 	const FacilitatorRef = RaiselyComponents.import('facilitator', { asRaw: true });
-	let Facilitator;
+	let Facilitator
 
 	return class ManageProfileLinks extends React.Component {
 		state = {};
@@ -12,10 +15,13 @@
 		async load() {
 			try {
 				if (!Facilitator) Facilitator = FacilitatorRef().html;
-				const [teams, profile] = await Promise.all([
+				const [teams, profile, authenticate] = await Promise.all([
 					Facilitator.getTeams(),
 					Facilitator.getFacilitatorProfile(this.props),
+					getData(api.users.authenticate())
 				]);
+				console.log('Authenticate', authenticate);
+				const isAdmin = get(authenticate, 'roles', []).includes('ORG_ADMIN');
 				this.setState({ profile, teams });
 			} catch (e) {
 				this.setState({ error: e.message || 'Failed to load' });
@@ -45,12 +51,25 @@
 			);
 		}
 
+		renderAdmin() {
+			return (
+				<div className="col col--4 manage-profile-block">
+					<h3>Program Coordinator Tools</h3>
+					<p>Manage teams, and communications templates.</p>
+					<Button href="/dashboards/admin">Administrator Tools</Button>
+				</div>
+			);
+		}
+
 		render() {
 			const { error, profile, teams } = this.state;
+			// FIXME set this once we've resolved facil and team leader permissions
+			const isAdmin = false;
 			return (
 				<div className="row__container manage-profile-wrapper">
 					{profile ? this.renderFacil(profile) : ''}
 					{teams && teams.length ? this.renderTeam(teams) : ''}
+					{isAdmin && this.renderAdmin() }
 					{error && (
 						<div className="error">
 							<p>There was an error loading your profiles:</p>
