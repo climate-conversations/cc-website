ccf750f291bb3cb7b66eb3e4b3dbd1eaf806368d
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-04-17 16:22:25 +1200

    Spec upsert with assignment

--- cc-proxy/spec/controllers/upsert.spec.js
+++ cc-proxy/spec/controllers/upsert.spec.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-param-reassign */
 const { pick } = require('lodash');
+const request = require('request-promise-cache');
 
 const chai = require('chai');
 const nock = require('nock');
@@ -42,6 +43,10 @@ describe('upsertUser', () => {
 		user: completeUser,
 	};
 	let raiselyRequest;
+
+	before(() => {
+		nock.cleanAll();
+	});
 
 	describe('new user', () => {
 		before(() => {
@@ -114,9 +119,128 @@ describe('upsertUser', () => {
 		statusOk(results);
 		itReturnsMinimalUser(results);
 	});
+
+	describe('assignment', () => {
+		let assignmentRequest;
+		describe('new user', () => {
+			before(() => {
+				results.user = { ...completeUser, uuid: 'some_other_uuid' };
+				setup(results, {
+					assignSelf: 1,
+					data: { ...createUser },
+				}, {
+					Authorization: 'Bearer 1234',
+				});
+				nock('https://api.raisely.com')
+					.post('/v3/users')
+					.reply(200, function userRequest(uri, body) {
+						raiselyRequest = {
+							body,
+							headers: this.req.headers,
+						};
+						return {
+							data: results.user,
+						};
+					})
+					.get('/v3/users?email=bob%40cc.test&private=1')
+					.reply(200, { data: [] })
+					.post('/v3/users/facil_uuid/assignments')
+					.reply(200, function assignmentReq(uri, body) {
+						assignmentRequest = {
+							body,
+							headers: this.req.headers,
+						};
+						return body;
+					});
+				nockAuthentication();
+
+				return upsertUser(results.req, results.res);
+			});
+			statusOk(results);
+			itReturnsMinimalUser(results);
+			it('posts user without falsey action', () => {
+				expect(raiselyRequest.body).to.eql({ data: filteredCreateUser });
+			});
+			it('assigns the user', () => {
+				expect(assignmentRequest).to.not.be.null;
+				expect(assignmentRequest.body).to.deep.eq({ data: [{ recordUuid: 'some_other_uuid', recordType: 'user' }] });
+			});
+		});
+		describe('existing user', () => {
+			before(() => {
+				request.cache.clear();
+				assignmentRequest = null;
+				setup(results, {
+					assignSelf: 1,
+					data: { ...createUser, uuid: 'some_other_uuid' },
+				}, {
+					Authorization: 'Bearer 1234',
+				});
+				nock('https://api.raisely.com')
+					.patch('/v3/users/some_other_uuid')
+					.reply(200, function userRequest(uri, body) {
+						raiselyRequest = {
+							body,
+							headers: this.req.headers,
+						};
+						return {
+							data: results.user,
+						};
+					})
+					.get('/v3/users?email=bob%40cc.test&private=1')
+					.reply(200, { data: [{ ...completeUser, uuid: 'some_other_uuid' }] })
+					.post('/v3/users/facil_uuid/assignments')
+					.reply(200, function assignmentReq(uri, body) {
+						assignmentRequest = {
+							body,
+							headers: this.req.headers,
+						};
+						return body;
+					});
+				nockAuthentication();
+
+				return upsertUser(results.req, results.res);
+			});
+			statusOk(results);
+			itReturnsMinimalUser(results);
+			it('assigns the user', () => {
+				expect(assignmentRequest).to.not.be.null;
+				expect(assignmentRequest.body).to.deep.eq({ data: [{ recordUuid: 'some_other_uuid', recordType: 'user' }] });
+			});
+		});
+		describe('unauthorized', () => {
+			before(() => {
+				request.cache.clear();
+				assignmentRequest = null;
+				setup(results, {
+					assignSelf: 1,
+					data: { ...createUser, uuid: 'some_other_uuid' },
+				});
+				nock('https://api.raisely.com')
+					.patch('/v3/users/some_other_uuid')
+					.reply(200, function userRequest(uri, body) {
+						raiselyRequest = {
+							body,
+							headers: this.req.headers,
+						};
+						return {
+							data: completeUser,
+						};
+					})
+					.get('/v3/users?email=bob%40cc.test&private=1')
+					.reply(200, { data: [{ ...completeUser, uuid: 'some_other_uuid' }] });
+				nockAuthentication();
+
+				return upsertUser(results.req, results.res);
+			});
+			it('fails', () => {
+				expect(results.res.statusCode).to.eq(403);
+			});
+		});
+	});
 });
 
-function prepareRequest(results, body) {
+function prepareRequest(results, body, headers = {}) {
 	results.res = new MockResponse();
 	results.req = new MockRequest({
 		body,
@@ -124,12 +248,30 @@ function prepareRequest(results, body) {
 		url: '/',
 		headers: {
 			Origin: 'https://climateconversations.raisely.com',
+			...headers,
 		},
 	});
 }
 
-function setup(results) {
-	return prepareRequest(results,  {
-		data: createUser,
-	});
+function setup(results, body, headers) {
+	let theBody = body || {
+		data: { ...createUser },
+	};
+	return prepareRequest(results, theBody, headers);
+}
+
+function nockAuthentication() {
+	nock('https://api.raisely.com')
+		.get('/v3/authenticate')
+		.reply(200, {
+			data: { roles: [] },
+		});
+	nock('https://api.raisely.com')
+		.get('/v3/users/me?private=1')
+		.reply(200, {
+			data: {
+				uuid: 'facil_uuid',
+				tags: [{ path: 'facilitator' }],
+			},
+		});
 }
