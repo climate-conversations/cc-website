95395f6cdd3f939a4e6c6148664a9fe8ae2ece6c
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-06-21 16:56:47 +1200

    Update to manual sync

--- conversation-sync/util/syncList.js
+++ conversation-sync/util/syncList.js
@@ -5,48 +5,15 @@ require('../config');
 const MailchimpController = require('../src/controllers/Mailchimp');
 const { raiselyRequest } = require('../src/helpers/raiselyHelpers');
 
-const CONCURRENCY=10;
+const CONCURRENCY=5;
 
 const controller = new MailchimpController({
 	log: console.log,
 });
-const limit = parseInt(process.env.LIMIT) || 100;
-let offset = parseInt(process.env.OFFSET) || 0;
 
-async function syncRaiselyDonorsToMailchimp() {
-	const body = await raiselyRequest({
-		path: `/donations?private=1&limit=${limit}&offset=${offset}`,
-		fullResult: true,
-		token: process.env.RAISELY_TOKEN,
-	});
-	let error;
-	let promises = [];
-
-	const donations = body.data;
-
-	for (let i = 0; i < donations.length; i++) {
-		const donation = donations[i];
-
-		const data = {
-			type: 'donation.created',
-			data: donation,
-		}
-		console.log(`**** Syncing donor ${i + offset} ${donation.uuid} ${donation.email} ****`);
-		const promise = controller.process({ data }).catch(err => error = err);
-		promises.push(promise);
-		promise.then(() => {
-			_.remove(promises, x => x === promise);
-		});
-
-		if (promises.length > CONCURRENCY) await Promise.race(promises);
-		if (error) throw error;
-	}
-
-	await Promise.all(promises);
-	if (error) throw error;
-}
-
-async function syncRaiselyToMailchimp() {
+async function paginateRaisely(url, callback) {
+	const limit = parseInt(process.env.LIMIT) || 100;
+	let offset = parseInt(process.env.OFFSET) || 0;
 	let next;
 
 	const promises = [];
@@ -56,24 +23,17 @@ async function syncRaiselyToMailchimp() {
 	do {
 		console.log('Fetching records from raisely ...');
 		const body = await raiselyRequest({
-			path: next || `/users?private=1&limit=${limit}&offset=${offset}`,
+			path: next || `${url}&limit=${limit}&offset=${offset}`,
 			fullResult: true,
 			token: process.env.RAISELY_TOKEN,
 		});
 
 		next = body.pagination.nextUrl;
-		const users = body.data;
+		const records = body.data;
 		console.log('nextUrl will be', next);
 
-		for (let i = 0; i < users.length; i++) {
-			const user = users[i];
-
-			const data = {
-				type: 'user.updated',
-				data: user,
-			}
-			console.log(`**** Syncing person ${i + offset} ${user.uuid} ${user.email} ****`);
-			const promise = controller.process({ data }).catch(err => error = err);
+		for (let i = 0; i < records.length; i++) {
+			const promise = callback(records[i], i + offset).catch(err => error = err);
 			promises.push(promise);
 			promise.then(() => {
 				_.remove(promises, x => x === promise);
@@ -87,11 +47,44 @@ async function syncRaiselyToMailchimp() {
 		if (error) throw error;
 
 	} while (next);
+	await Promise.all(promises);
 	if (error) throw error;
 }
 
-syncRaiselyDonorsToMailchimp()
-	.catch(console.error);
+async function syncRaiselyDonorsToMailchimp() {
+	let donorUuids = [];
 
-// syncRaiselyToMailchimp()
-// 	.catch(console.error);
+	return paginateRaisely('/donations?private=1', async (donation, i) => {
+		if (donation.mode === 'TEST') return;
+		if (donorUuids.includes(donation.user.uuid)) {
+			console.log('Recurring donor has already been added, nothing to do');
+			return;
+		}
+		donorUuids.push(donation.user.uuid);
+		const data = {
+			type: 'donation.created',
+			data: donation,
+		}
+		console.log(`**** Syncing donor ${i} ${donation.user.uuid} ${donation.email} ****`);
+		return controller.process({ data });
+	});
+}
+
+async function syncRaiselyToMailchimp() {
+	return paginateRaisely('/users?private=1', async (user, i) => {
+		const data = {
+			type: 'user.updated',
+			data: user,
+		}
+		if (user.email !== 'hills.gemma@gmail.com') return;
+		console.log(`**** Syncing person ${i} ${user.uuid} ${user.email} ****`);
+		return controller.process({ data });
+	});
+}
+
+async function doAll() {
+	// await syncRaiselyDonorsToMailchimp();
+	await syncRaiselyToMailchimp();
+}
+
+doAll().catch(console.error);
