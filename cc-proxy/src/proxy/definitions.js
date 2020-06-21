f601fb16550be5c4d2b261811f7edb39a2bc6303
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-06-21 16:51:23 +1200

    Update to permissions

--- cc-proxy/src/proxy/definitions.js
+++ cc-proxy/src/proxy/definitions.js
@@ -12,82 +12,62 @@
  */
 
 const { minimalUser } = require('./transforms');
-const { searchUsers, isUserAssignment, isAssignedUser } = require('./conditions');
+const { searchUsers, isUserAssignment, isAssignedUser, isConversationScoped } = require('./conditions');
+
+// Actions that facils need escalation for but team leaders don't
+const facilitatorEscalations = [
+	// Allow facilitators to retrieve contact details for their team profile
+	{ method: 'GET', path: /\/profiles\/[a-zA-Z0-9_-]*/, tags: ['facilitator'],	/* condition: isAssignedUser */ },
+	// Allow facilitators to assign records they are already to
+	{ method: 'POST', path: /\/users\/[a-zA-Z0-9_-]*\/assignments/, tags: ['facilitator'], /* condition: isAssignedUser */ },
+	{ method: 'GET', path: '/search', tags: ['facilitator'], condition: searchUsers, transform: minimalUser },
+	{ method: 'GET', path: /\/interactions\/.*/, tags: ['facilitator'], /* condition: isAssignedUser */ },
+	{ method: 'POST', path: /\/interactions\/.*/, tags: ['facilitator'], /* condition: isAssignedUser */ },
+];
+// If someone is a team leader and a facil, set up rules that pass through using their
+// permissions rather than escalating
+const teamLeaderNonEscalations = facilitatorEscalations.map(rule => ({
+	...rule,
+	tags: ['team-leader'],
+	noEscalate: true,
+}))
 
 module.exports = [
-{
 	// Let team leaders create facilitators
-	method: 'POST',
-	path: '/setupVolunteer/facilitator',
-	tags: ['team-leader'],
-},
-{
+	{ method: 'POST', path: '/setupVolunteer/facilitator', tags: ['team-leader'] },
 	// Let ORG_ADMINs create team leaders
-	method: 'POST',
-	path: '/setupVolunteer/team-leader',
-	roles: ['ORG_ADMIN'],
-},
-{
+	{ method: 'POST', path: '/setupVolunteer/team-leader', roles: ['ORG_ADMIN'] },
 	// Let facilitators retrieve the message templates stored on
 	// campaign.private
-	method: 'GET',
-	path: /\/campaigns\/[a-zA-Z0-9_-]*/,
-	tags: ['team-leader', 'facilitator'],
-},
-{
+	{ method: 'GET', path: /\/campaigns\/[a-zA-Z0-9_-]*/, tags: ['team-leader', 'facilitator'] },
 	// Let facils and leaders assign records
-	method: 'POST',
-	path: '/assignments',
-	tags: ['team-leader', 'facilitator'],
-},
-/***** Let the public sign up for things ****** */
-{
+	{ method: 'POST', path: '/assignments', tags: ['team-leader', 'facilitator'] },
+
+	/***** Let the public sign up for things ****** */
 	// Let the public signup
-	method: 'POST',
-	path: '/interactions',
-	// condition: onlyCertainCategories
-}, {
+	{ method: 'POST', path: '/interactions', /* condition: onlyCertainCategories */ },
 	// Let the public RSVP
-	method: 'POST',
-	path: /\/events\/[a-zA-Z0-9_-]*\/rsvps/,
-	// condition: onlyCertainCategories
-},
-/***** Let the public sign up for things ****** */
-{
-	method: 'GET',
-	path: /\/interactions\/.*/,
-	tags: ['team-leader'],
-}, {
-	method: 'POST',
-	path: /\/users\/[a-zA-Z0-9_-]*\/assignments/,
-	tags: ['team-leader'],
-	// Only allow team-leaders to assign users (not other record types)
-	condition: isUserAssignment,
-},
-/****** Allow facilitators to retrieve contact details for their team profile */
-{
-	method: 'GET',
-	path: /\/profiles\/[a-zA-Z0-9_-]*/,
-	tags: ['facilitator'],
-	// condition: isAssignedUser,
-},
-{
-	method: 'POST',
-	path: /\/users\/[a-zA-Z0-9_-]*\/assignments/,
-	tags: ['facilitator'],
-	// Only allow facilitators to assign records they are already
-	// themselves assigned to
-	condition: isAssignedUser,
-}, {
-	method: 'POST',
-	path: /\/events\/[a-zA-Z0-9_-]*\/eventRsvp/,
-	tags: ['facilitator', 'team-leader'],
-}, {
-	method: 'GET',
-	// path: '/search',
-	tags: ['facilitator', 'team-leader'],
-	// Only allow search for user records
-	condition: searchUsers,
-	// Transform the results to be the minimum attributes needed on the users
-	transform: minimalUser,
-}];
+	{ method: 'POST', path: /\/events\/[a-zA-Z0-9_-]*\/rsvps/, /* condition: onlyCertainCategories */ },
+
+	/***** Skip escalation for team leader actions ****** */
+	{ method: 'GET', path: /\/interactions\/.*/, tags: ['team-leader'], noEscalate: true },
+	{ method: 'POST', path: /\/users\/[a-zA-Z0-9_-]*\/assignments/,	tags: ['team-leader'], noEscalate: true },
+
+	/** Facilitators */
+	...teamLeaderNonEscalations,
+	...facilitatorEscalations,
+
+	// Manage donations
+	{ method: 'POST', path: /\/donations/, tags: ['facilitator'] },
+	{ method: 'GET', path: /\/donations.*/, tags: ['facilitator'], condition: isConversationScoped },
+	{ method: 'GET', path: /\/donations.*/, tags: ['team-leader'], condition: isConversationScoped },
+
+	/** Conversation management */
+	{ method: 'POST', path: /\/events.*/, tags: ['facilitator', 'team-leader'] },
+	{ method: 'PATCH', path: /\/events\/.*/, tags: ['facilitator', 'team-leader'] },
+	{ method: 'PUT', path: /\/events\/.*/, tags: ['facilitator', 'team-leader'] },
+	{ method: 'GET', path: /\/events.*/, tags: ['facilitator', 'team-leader'] },
+	{ method: 'GET', path: /\/event_rsvps.*/, tags: ['facilitator', 'team-leader'] },
+	{ method: 'POST', path: '/event_rsvps', tags: ['facilitator', 'team-leader'] },
+	{ method: 'PUT', path: /\/event_rsvps\/.*/, tags: ['facilitator', 'team-leader'] },
+];
