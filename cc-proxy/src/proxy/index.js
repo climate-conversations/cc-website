c9e5a46d736902e2434e07d208df30a9ab082f1a
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-06-21 16:52:06 +1200

    Support rules that permit pass through but don't escalate

--- cc-proxy/src/proxy/index.js
+++ cc-proxy/src/proxy/index.js
@@ -1,19 +1,30 @@
+const _ = require('lodash');
 const { authorize } = require('./permissions');
 const raisely = require('../raiselyRequest');
 
-module.exports = async function proxy(req) {
+function printArray(source, path) {
+	return _.get(source, path, []).join(',');
+}
+
+module.exports = async function proxy(req, res) {
 	const [path] = req.originalUrl.split('?');
 
 	const rule = await authorize(req, path);
 
+	const escalate = !!rule && !rule.noEscalate;
 	const options = {
 		path: req.originalUrl,
 		query: req.query,
 		body: req.body,
 		method: req.method,
-		// Explicitly request escalation
-		escalate: !!rule,
+		// Explicitly request escalation if there's a
+		// matching rule and it doesn't suggest no escalation
+		escalate,
 	};
+
+	res.set('X-CC-Proxy-Escalate', escalate ? 'Yes' : 'No');
+	res.set('X-CC-Proxy-Roles', escalate ? 'ORG_ADMIN' : printArray(rule, 'originalUser.roles'))
+	res.set('X-CC-Rule-Matched-On', `${printArray(rule, 'tags')} ${printArray(rule, 'roles')}`);
 
 	if (rule && rule.transform) {
 		options.transform = rule.transform;
