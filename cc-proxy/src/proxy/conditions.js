c0fec0e6e494767eeaaa5a73b257bc0d6df5f7fe
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-06-21 16:50:58 +1200

    Conversation scope helper

--- cc-proxy/src/proxy/conditions.js
+++ cc-proxy/src/proxy/conditions.js
@@ -1,5 +1,19 @@
 const _ = require('lodash');
+const qs = require('qs');
 const RestError = require('../restError');
+
+function parsePath(url) {
+	const split = url.indexOf('?');
+	const path = split === -1 ? url : url.slice(0, split);
+	const query = split === -1 ? '' : url.slice(split + 1, url.length);
+
+	return { path, query };
+}
+
+function getQuery(url) {
+	const { path, query } = parsePath(url);
+	return qs.parse(query);
+}
 
 function notImplemented() {
 	throw new Error('Not implemented');
@@ -17,8 +31,14 @@ function isUserAssignment(req) {
 	return assignments.reduce((all, curr) => all && curr.recordType === 'user', true);
 }
 
+function isConversationScoped(req) {
+	const query = getQuery(req.originalUrl);
+	return !!query.conversationUuid;
+}
+
 module.exports = {
 	onlyUsers: notImplemented,
 	isUserAssignment,
 	isAssignedUser: notImplemented,
+	isConversationScoped,
 };
