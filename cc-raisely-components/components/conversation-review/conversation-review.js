04795b3a585ddf1d074193e93437af97a2ee7bdc
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-08-12 23:27:30 +1200

    Full implementation of review

--- cc-raisely-components/components/conversation-review/conversation-review.js
+++ cc-raisely-components/components/conversation-review/conversation-review.js
@@ -1,15 +1,20 @@
 (RaiselyComponents, React) => {
 	const { displayCurrency, get } = RaiselyComponents.Common;
+	const { Spinner } = RaiselyComponents;
 
 	const Checkbox = RaiselyComponents.import('checkbox');
+	const ReviewStamp = RaiselyComponents.import("reviewed-stamp");
 	const ReturnButton = RaiselyComponents.import('return-button');
 	const WhatsAppButton = RaiselyComponents.import('whatsapp-button');
 	const ConversationRef = RaiselyComponents.import('conversation', { asRaw: true });
+	const UserSaveHelperRef = RaiselyComponents.import('cc-user-save', { asRaw: true });
 	let Conversation;
+	let UserSaveHelper;
 
 	return class ConversationReview extends React.Component {
 		state = { facilitators: [], reflections: [] };
 		componentDidMount() {
+			this.setState({ loading: true });
 			this.load();
 		}
 		componentDidUpdate() {
@@ -21,44 +26,86 @@
 				this.load();
 			}
 		}
+		async countFacilConversations(user) {
+			const results = await Conversation.loadRsvps({
+				query: {
+						type: 'facilitator,co-facilitator',
+						user: user.uuid,
+						['event.startAtLT']: new Date().toISOString(),
+					},
+				});
+			return {
+				uuid: user.uuid,
+				count: results.rsvps.length,
+			}
+		}
+
 		async load() {
+			const { props } = this;
 			try {
 				if (!Conversation) Conversation = ConversationRef().html;
 				const eventUuid = Conversation.getUuid(this.props);
 				this.setState({ eventUuid });
-				const conversationPromise = Conversation.loadConversation({ props: this.props, private: true })
-					.then(conversation => this.setState({ conversation }));
 
 				const [results, conversation, reflectionsArray] = await Promise.all([
 					Conversation.loadRsvps({ props: { eventUuid }, type: ['co-facilitator', 'facilitator', 'guest']}),
-					Conversation.updateStatCache(eventUuid),
+					Conversation.updateStatCache({ props, eventUuid }),
 					Conversation.loadReflections({ eventUuid }),
-					conversationPromise,
 				]);
+				const allFacils = results.facilitators.concat(results['co-facilitators']);
+				console.log('results', results, allFacils)
+				const conversationCountPromises = allFacils.map(facil => this.countFacilConversations(facil));
+
 				const reflections = {};
 				reflectionsArray.forEach(r => reflections[r.userUuid] = r);
-				this.setState({ ...results, conversation, reflections, lodaded: true }, this.calculateConversation);
+				this.setState({ ...results, conversation, reflections, loading: false }, this.calculateConversation);
+
+				const conversationCounts = await Promise.all(conversationCountPromises);
+				const facilConversations = {};
+				conversationCounts.forEach(c => facilConversations[c.uuid] = c.count);
+				this.setState({ facilConversations });
 			} catch (e) {
+				console.error(e);
 				this.setState({ error: e.message || 'Unknown error', loading: false })
 			}
 		}
 
-		calculateConversation = async () => {
-			const { facilitators } = this.state;
-			facilitators.forEach(f => {
-
-			})
-		}
-
-		row({ label, values }) {
+		row({ label, values, space, line }) {
+			let rowClass = `data-row`;
+			if (space) rowClass += ' space';
+			if (line) rowClass += ' line';
 			return (
-				<div key={label} className="data-row">
+				<div key={label} className={rowClass}>
 					<div className="data-field-label">{label}</div>
 					{values.map((value, index) => (
 						<div key={index} className="data-field-value">{value}</div>
 					))}
 				</div>
-			)
+			);
+		}
+
+		markReviewComplete = async () => {
+			const { conversation } = this.state;
+			this.setState({ saving: true });
+			try {
+				const reviewSettings = {
+					reviewedBy: get(this.props, 'global.user.uuid'),
+					reviewedAt: new Date().toISOString(),
+				};
+				Object.assign(conversation.private, reviewSettings);
+				if (!UserSaveHelper) UserSaveHelper = UserSaveHelperRef().html;
+				await UserSaveHelper.proxy(`/events/${conversation.uuid}`, {
+					method: 'PATCH',
+					body: {
+						data: { private: reviewSettings },
+						partial: true,
+					}
+				});
+				this.setState({ saving: false });
+			} catch (e) {
+				console.error(e);
+				this.setState({ error: e.message || 'Unknown error', saving: false })
+			}
 		}
 
 		checkMainStats(placeHolder) {
@@ -68,7 +115,7 @@
 				hosts: 'Host Interest',
 				facilitators: 'Facil Interest',
 			};
-			const rows = ['guests', 'hosts', 'faciltiators'].map(key => {
+			const rows = ['guests', 'hosts', 'facilitators'].map(key => {
 				const eventVal = get(conversation, 'private.statCache.guests', placeHolder);
 				let checkText = facilitators
 					.map(f => {
@@ -85,10 +132,11 @@
 					.filter(n => n)
 					.join(', ');
 				const label = labels[key];
+				if (checkText.length) checkText = `(${checkText})`;
 
 				return { label, values: [
 					eventVal, (
-						<span key={key} className="conversation-review__check">({checkText})</span>
+						<span key={key} className="conversation-review__check">{checkText}</span>
 					),
 				]};
 			})
@@ -96,32 +144,69 @@
 		}
 
 		render() {
-			const { facilitators, conversation, loaded } = this.state;
-			const placeHolder = loaded ? 0 : 'Loading...'
-			let donations = get(conversation, 'private.statCache.donations.totalAmount', placeHolder);
-			if (typeof donations !== 'string') donations = displayCurrency(donations);
+			const { facilitators, conversation, loading, error, saving } = this.state;
+			const facilConversations = this.state.facilConversations || {};
+			const placeHolder = loading ? ' ' : 0;
+
+			const totalDonations = get(conversation, 'private.statCache.donations.total', placeHolder);
+			const onlineDonations = get(conversation, 'private.statCache.donations.online', placeHolder);
+			const cashDonations = get(conversation, 'private.statCache.donations.cash', placeHolder);
+			let donationValues = [cashDonations, onlineDonations, totalDonations]
+			donationValues = donationValues.map((d, i) => {
+				const val = displayCurrency(d, 'SGD');
+				const label = ['cash (reported)', 'online', 'total'];
+				return `${val} ${label[i]}`;
+			});
+
 			const conversationValues = this.checkMainStats(placeHolder);
 			const facilitatorValues = [
-				{ label: 'Facilitator', values: facilitators.map(f => f.fullName || f.preferredName) },
-				{ label: 'Conversation #', values: [] },
+				{ label: '', values: facilitators.map(f => f.fullName || f.preferredName) },
+				{ label: 'Conversations', values: facilitators.map(f => facilConversations[f.uuid] ) },
 				{ label: 'Contact', values: facilitators.map(f => <WhatsAppButton key={f.phoneNumber} phone={f.phoneNumber} />) },
 			];
 			if (!Conversation) Conversation = ConversationRef().html;
 			const isReconciled = Conversation.isReconciled(conversation);
 
+			if (error) {
+				return <div className="error"><p>There was an error loading</p></div>;
+			}
+
+			const { reviewedAt } = get(conversation, 'private', {});
+
 			return (
 				<div className="conversation-review">
-					{conversationValues.map(row => this.row(row))}
-					{this.row({ label: 'Donations', values: [donations] })}
-					{facilitatorValues.map(row => this.row(row))}
-					{!isReconciled ? (
-						<p>You {"can't"} check this box until donation reconciliation is complete</p>
-					) : ''}
-					<Checkbox
-						label='I have reviewied this conversation and it is fully processed'
-						disabled={!isReconciled}
-					/>
-					<ReturnButton {...this.props} saveLabel="Done" />
+					<h3>{get(conversation, 'name', '')}</h3>
+					{loading ? <Spinner/ > : (
+						<div className="conversation-review__data">
+							{conversationValues.map((row, i) => this.row({ ...row, lines: true }))}
+							{this.row({ label: 'Donations', values: donationValues, space: true })}
+							<h4>Facilitators</h4>
+							{facilitatorValues.map(row => this.row(row))}
+						</div>
+					)}
+					<div className="conversation-review__checkbox">
+						{saving ? (
+							<Spinner />
+						) : (
+							<React.Fragment>
+								{reviewedAt ? (
+									<ReviewStamp conversation={conversation} />
+								) : (
+									<React.Fragment>
+										{!isReconciled ? (
+											<p>You {"can't"} check this box until donation reconciliation is complete</p>
+										) : ''}
+										<Checkbox
+											label='I have reviewied this conversation and it is fully processed'
+											disabled={!(isReconciled || !loading)}
+											onChange={this.markReviewComplete}
+										/>
+									</React.Fragment>
+								)}
+							</React.Fragment>
+						)}
+					</div>
+					<ReturnButton {...this.props} backLabel={reviewedAt ? 'Go Back' : 'Done'} />
 				</div>
 			);
 		}
