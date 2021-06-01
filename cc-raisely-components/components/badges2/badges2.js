08554c1c0c957eb65d76891b5bce49d0b4b0cf8d
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2021-06-02 09:21:49 +1200

    Reformat

--- cc-raisely-components/components/badges2/badges2.js
+++ cc-raisely-components/components/badges2/badges2.js
@@ -1,7 +1,7 @@
 /**
-  * A list of badge ids to display
-  * @field {text} badges one or more of motivation,recruit,host,lung
-  */
+ * A list of badge ids to display
+ * @field {text} badges one or more of motivation,recruit,host,lung
+ */
 
 // eslint-disable-next-line no-unused-expressions
 (RaiselyComponents, React) => {
@@ -11,44 +11,60 @@
 	 * returns true or false indicating if the badge
 	 * is active for the current profile
 	 */
-	const badgeConfig = [{
-		id: 'switched',
-		name: 'Switched On!',
-		isActive: p => p && p.public && p.public.switchOnConfirmed,
-		// Images for active/inactive badge
-		active: 'https://raisely-images.imgix.net/switchon/uploads/badge-switchon-2-png.png',
-		inactive: 'https://raisely-images.imgix.net/switchon/uploads/badge-switchon-1-png.png',
-		description: 'Has switched themselves to green electricity',
-	}, {
-		id: 'motivation',
-		name: 'Shared Why',
-		isActive: p => p && p.description,
-		// Images for active/inactive badge
-		active: 'https://raisely-images.imgix.net/switchon/uploads/badge-sharedmotivation-2-png.png',
-		inactive: 'https://raisely-images.imgix.net/switchon/uploads/badge-sharedmotivation-1-png.png',
-		description: "Has shared why they're acting",
-	}, {
-		id: 'recruit',
-		name: 'Recruited a Friend',
-		isActive: (p, state) => state.recruited,
-		active: 'https://raisely-images.imgix.net/switchon/uploads/badge-switchon-2-png.png',
-		inactive: 'https://raisely-images.imgix.net/switchon/uploads/badge-switchon-1-png.png',
-		description: 'Has successfully recruited 1 friend',
-	}, {
-		id: 'host',
-		name: 'Inspiration',
-		isActive: p => p && p.public && p.public.hosted,
-		active: 'https://raisely-images.imgix.net/switchon/uploads/badge-inspiration-2-png.png',
-		inactive: 'https://raisely-images.imgix.net/switchon/uploads/badge-inspiration-1-2-png.png',
-		description: 'Has hosted a climate conversation',
-	}, {
-		id: 'lung',
-		name: 'Lung Saver',
-		isActive: (p, state) => (state.recruited >= 5),
-		active: 'https://raisely-images.imgix.net/switchon/uploads/badge-lungsaver-2-png.png',
-		inactive: 'https://raisely-images.imgix.net/switchon/uploads/badge-lungsaver-1-2-png.png',
-		description: 'Has switched 5 or more people!',
-	}];
+	const badgeConfig = [
+		{
+			id: "switched",
+			name: "Switched On!",
+			isActive: p => p && p.public && p.public.switchOnConfirmed,
+			// Images for active/inactive badge
+			active:
+				"https://raisely-images.imgix.net/switchon/uploads/badge-switchon-2-png.png",
+			inactive:
+				"https://raisely-images.imgix.net/switchon/uploads/badge-switchon-1-png.png",
+			description: "Has switched themselves to green electricity"
+		},
+		{
+			id: "motivation",
+			name: "Shared Why",
+			isActive: p => p && p.description,
+			// Images for active/inactive badge
+			active:
+				"https://raisely-images.imgix.net/switchon/uploads/badge-sharedmotivation-2-png.png",
+			inactive:
+				"https://raisely-images.imgix.net/switchon/uploads/badge-sharedmotivation-1-png.png",
+			description: "Has shared why they're acting"
+		},
+		{
+			id: "recruit",
+			name: "Recruited a Friend",
+			isActive: (p, state) => state.recruited,
+			active:
+				"https://raisely-images.imgix.net/switchon/uploads/badge-switchon-2-png.png",
+			inactive:
+				"https://raisely-images.imgix.net/switchon/uploads/badge-switchon-1-png.png",
+			description: "Has successfully recruited 1 friend"
+		},
+		{
+			id: "host",
+			name: "Inspiration",
+			isActive: p => p && p.public && p.public.hosted,
+			active:
+				"https://raisely-images.imgix.net/switchon/uploads/badge-inspiration-2-png.png",
+			inactive:
+				"https://raisely-images.imgix.net/switchon/uploads/badge-inspiration-1-2-png.png",
+			description: "Has hosted a climate conversation"
+		},
+		{
+			id: "lung",
+			name: "Lung Saver",
+			isActive: (p, state) => state.recruited >= 5,
+			active:
+				"https://raisely-images.imgix.net/switchon/uploads/badge-lungsaver-2-png.png",
+			inactive:
+				"https://raisely-images.imgix.net/switchon/uploads/badge-lungsaver-1-2-png.png",
+			description: "Has switched 5 or more people!"
+		}
+	];
 
 	/**
 	 * Once you've declared your required components, be sure to return the class
@@ -56,29 +72,32 @@
 	 */
 	return class BadgesComponent extends React.Component {
 		state = {
-			recruited: null,
-		}
+			recruited: null
+		};
 
 		/**
-		  * Load any additional information needed for the badges
-		  */
-		componentDidMount() {
-		}
+		 * Load any additional information needed for the badges
+		 */
+		componentDidMount() {}
 
 		/**
-		  * This fetches the number of profiles that are membes of this profile
-		  * which can then be used to create badges around how many people are recruited
-		  */
+		 * This fetches the number of profiles that are membes of this profile
+		 * which can then be used to create badges around how many people are recruited
+		 */
 		loadRecruits(profile) {
 			if (!profile) return;
 
 			// Only reload if the state has yet to be fetched
 			if (this.state.recruited === null) {
-				fetch(`https://api.raisely.com/v3/profiles/${profile.uuid}/members?public.switchOnConfirmed=yes`)
+				fetch(
+					`https://api.raisely.com/v3/profiles/${
+						profile.uuid
+					}/members?public.switchOnConfirmed=yes`
+				)
 					.then(response => response.json())
-					.then((body) => {
+					.then(body => {
 						if (!body.pagination) {
-							console.log('Error fetching recruits', body);
+							console.log("Error fetching recruits", body);
 						} else {
 							this.setState({ recruited: body.pagination.total });
 						}
@@ -87,7 +106,10 @@
 		}
 
 		render() {
-			const profile = this.props.global.current.profile || this.props.global.user.profile || {};
+			const profile =
+				this.props.global.current.profile ||
+				this.props.global.user.profile ||
+				{};
 
 			const values = this.props.getValues();
 
@@ -95,16 +117,18 @@
 
 			// Show selected badges, or all badges
 			values.badges =
-				(values.badges && values.badges.split(',').map(b => b.trim())) ||
+				(values.badges &&
+					values.badges.split(",").map(b => b.trim())) ||
 				badgeConfig.map(b => b.id);
 
 			const badges = badgeConfig
 				// Show only selected badges
 				.filter(badge => values.badges.includes(badge.id))
 				// render each selected badge
-				.map((badge) => {
+				.map(badge => {
 					let isActive = false;
-					if (badge.isActive) isActive = badge.isActive(profile, this.state);
+					if (badge.isActive)
+						isActive = badge.isActive(profile, this.state);
 					const url = isActive ? badge.active : badge.inactive;
 
 					const fullUrl = `${url}?w=200`;
@@ -113,22 +137,21 @@
 					// element in this array. see: https://reactjs.org/docs/lists-and-keys.html
 					return (
 						<div
-							className={`badge ${isActive ? 'active' : 'inactive'}`}
+							className={`badge ${
+								isActive ? "active" : "inactive"
+							}`}
 						>
 							<img
 								src={fullUrl}
 								key={badge.name}
-								alt={badge.description} />
+								alt={badge.description}
+							/>
 							<h5>{badge.name}</h5>
 						</div>
 					);
 				});
 
-			return (
-				<div className="badges">
-					{badges}
-				</div>
-			);
+			return <div className="badges">{badges}</div>;
 		}
 	};
 };
