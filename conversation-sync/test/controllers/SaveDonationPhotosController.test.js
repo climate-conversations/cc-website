81738eea8d07cadf4ef4410012bf63df7aa2e233
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2021-07-10 21:54:27 +1200

    Fix broken tests

--- conversation-sync/test/controllers/SaveDonationPhotosController.test.js
+++ conversation-sync/test/controllers/SaveDonationPhotosController.test.js
@@ -1,11 +1,11 @@
-const requestPromiseCache = require("request-promise-cache");
-const sinon = require("sinon");
-const { expect } = require("chai");
+const requestPromiseCache = require('request-promise-cache');
+const sinon = require('sinon');
+const { expect } = require('chai');
 
-const { nockEventTeam } = require("../testHelper");
-const SavePhotoControler = require("../../src/controllers/SaveDonationPhotosController");
+const { nockEventTeam } = require('../testHelper');
+const SavePhotoControler = require('../../src/controllers/SaveDonationPhotosController');
 
-const mockDrive = require("../helpers/mockDrive");
+const mockDrive = require('../helpers/mockDrive');
 
 const WITH_MOCK = !process.env.LIVE_TEST;
 
@@ -14,29 +14,29 @@ let host;
 let sandbox;
 
 const photoUrl =
-	"https://raisely-images.imgix.net/uploads/screenshot-2020-02-18-at-10-04-39-am-png-7a91fe.png";
+	'https://raisely-images.imgix.net/uploads/screenshot-2020-02-18-at-10-04-39-am-png-7a91fe.png';
 
-describe("Save Donations Photo Controller", () => {
+describe('Save Donations Photo Controller', () => {
 	let controller;
 	let res;
 	let mockedDrive;
 
 	before(() => {
 		process.env.GOOGLE_PROJECT_CREDENTIALS =
-			"./test/controllers/fixtures/mockDriveCreds.json";
+			'./test/controllers/fixtures/mockDriveCreds.json';
 
 		requestPromiseCache.cache.clear();
 		controller = new SavePhotoControler({
 			log: console.log,
 		});
 	});
-	describe("Report photo", () => {
+	describe('Report photo', () => {
 		before(async () => {
 			mockedDrive = mockGoogle();
 			res = await processController({
 				conversation: {
-					uuid: "fake-uuid",
-					startAt: "2019-04-15T12:00:00Z",
+					uuid: 'fake-uuid',
+					startAt: '2019-04-15T12:00:00Z',
 				},
 				url: photoUrl,
 				type: 'report',
@@ -45,15 +45,17 @@ describe("Save Donations Photo Controller", () => {
 		after(() => {
 			sandbox.restore();
 		});
-		itSucceeds('2019-04-15 - report - host Test Host - facil Test Facilitator');
+		itSucceeds(
+			'2019-04-15 - fake-uuid - report - host Test Host - facil Test Facilitator'
+		);
 	});
-	describe("Transfer photo", () => {
+	describe('Transfer photo', () => {
 		before(async () => {
 			mockedDrive = mockGoogle();
 			res = await processController({
 				conversation: {
-					uuid: "fake-uuid",
-					startAt: "2019-04-14T23:30:00Z",
+					uuid: 'fake-uuid',
+					startAt: '2019-04-14T23:30:00Z',
 				},
 				url: photoUrl,
 				type: 'transfer',
@@ -62,15 +64,17 @@ describe("Save Donations Photo Controller", () => {
 		after(() => {
 			sandbox.restore();
 		});
-		itSucceeds('2019-04-15 - transfer - host Test Host - facil Test Facilitator')
+		itSucceeds(
+			'2019-04-15 - fake-uuid - transfer - host Test Host - facil Test Facilitator'
+		);
 	});
 	function itSucceeds(expectedName) {
-		it("sets photo name", () => {
+		it('sets photo name', () => {
 			expect(res.data.name).to.eq(expectedName);
 		});
 		if (WITH_MOCK) {
-			it("uploads photo", () => {
-				expect(mockedDrive.calls["files.create"]).to.containSubset([
+			it('uploads photo', () => {
+				expect(mockedDrive.calls['files.create']).to.containSubset([
 					{
 						name: expectedName,
 					},
@@ -82,14 +86,14 @@ describe("Save Donations Photo Controller", () => {
 	async function processController(data) {
 		({ facilitator, host } = nockEventTeam());
 		const result = await controller.process({
-			data: { type: "conversation.donationReportUploaded", data },
+			data: { type: 'conversation.donationReportUploaded', data },
 		});
 		return result;
 	}
 
 	function mockGoogle() {
 		sandbox = sinon.createSandbox();
-		if (!WITH_MOCK) console.log("CONNECTING TO LIVE GOOGLE DRIVE");
+		if (!WITH_MOCK) console.log('CONNECTING TO LIVE GOOGLE DRIVE');
 		return WITH_MOCK ? mockDrive(sandbox) : null;
 	}
 });
