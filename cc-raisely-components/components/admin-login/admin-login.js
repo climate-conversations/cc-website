dd3f32b72b838bb1fde1474ae2c451d7a4e25b63
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2021-07-19 20:25:25 +1200

    Set class to indicate components have loaded for login

--- cc-raisely-components/components/admin-login/admin-login.js
+++ cc-raisely-components/components/admin-login/admin-login.js
@@ -2,9 +2,14 @@
  * Logs the user in with a full admin login token so they can access all
  * features of the API through the portal
  */
-(RaiselyComponents, React) => class AdminLogin extends React.Component {
-	render() {
-		const LoginForm = window.CustomComponentRaiselyLoginForm.html;
-		return <LoginForm requestAdminToken {...this.props} />
-	}
-}
+(RaiselyComponents, React) =>
+	class AdminLogin extends React.Component {
+		render() {
+			if (!document.body.classList.contains('components-loaded')) {
+				document.body.classList.add('components-loaded');
+			}
+
+			const LoginForm = window.CustomComponentRaiselyLoginForm.html;
+			return <LoginForm requestAdminToken {...this.props} />;
+		}
+	};
