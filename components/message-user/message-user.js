f3e4029fb23e5590fb9e5c8906540fedc0a05ff6
KangJingA <66071629+KangJingA@users.noreply.github.com>
2021-08-03 13:23:03 +0800

    delete components folder

--- components/message-user/message-user.js
+++
@@ -1,6 +0,0 @@
-(RaiselyComponents) => class MyRaiselyComponent extends React.Component {
-	render() {
-		const values = this.props.getValues();
-		return <h1>This is a boilerplate Raisely custom component</h1>;
-	}
-}
