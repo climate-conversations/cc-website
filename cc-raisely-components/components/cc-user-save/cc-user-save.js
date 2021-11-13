fb88489e038dbe6b5a1802a819d618eccbd8dac9
KangJingA <66071629+KangJingA@users.noreply.github.com>
2021-11-13 10:29:50 +0800

    update  proxyHost

--- cc-raisely-components/components/cc-user-save/cc-user-save.js
+++ cc-raisely-components/components/cc-user-save/cc-user-save.js
@@ -40,8 +40,7 @@
 	const proxyHost = {
 		production:
 			'https://asia-northeast1-climate-conversations-sg-2019.cloudfunctions.net',
-		staging:
-			'https://asia-northeast1-cc-website-staging.cloudfunctions.net',
+		staging: 'https://us-central1-cc-website-staging.cloudfunctions.net',
 		development: 'http://localhost:3501',
 	}[mode];
 
