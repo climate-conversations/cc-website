58eb32ad95dec7f75a226c0ce7613e10f20ca9f4
KangJingA <66071629+KangJingA@users.noreply.github.com>
2022-01-27 07:46:09 +0800

    Update uniqueDonors.spec.js

--- cc-proxy/spec/controllers/uniqueDonors.spec.js
+++ cc-proxy/spec/controllers/uniqueDonors.spec.js
@@ -8,9 +8,23 @@ require('../spec.env.js');
 const MockResponse = require('../utils/mockResponse');
 const MockRequest = require('../utils/mockRequest');
 const { uniqueDonors } = require('../../src');
+const { before } = require('lodash');
 const { expect } = chai;
+// before block to sets off the nocks
+// several it statements to check:
+// wrong campaignUuid
+// wrong uuid
+// check for the right respons
 
 describe('wrong campaignUuid', () => {
+	let patchBody;
+	before(() => {
+		nock(RAISELY_API)
+			.post('/v3/profiles/${UUID}')
+			.reply((uri, body) => {
+				patchBody = body; // to check body contains no duplicated email (donor count)
+			});
+	});
 	// some filler code
 	it('it works', () => {
 		const a = 1;
@@ -25,8 +39,7 @@ describe('wrong campaignUuid', () => {
 // expect response to be equal to the mock response
 
 // test cases:
-// wrong campaignUuid
-// wrong uuid
+
 // single donor
 // multiple donors
 // multiple donors with repeated emails
