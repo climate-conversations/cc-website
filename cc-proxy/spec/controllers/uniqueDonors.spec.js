cc8a78248ca7e12352fb4134c42f352012c3c154
KangJingA <66071629+KangJingA@users.noreply.github.com>
2022-01-29 22:55:42 +0800

    configure mock request and init mock AllDonations

--- cc-proxy/spec/controllers/uniqueDonors.spec.js
+++ cc-proxy/spec/controllers/uniqueDonors.spec.js
@@ -7,8 +7,7 @@ require('../spec.env.js');
 
 const MockResponse = require('../utils/mockResponse');
 const MockRequest = require('../utils/mockRequest');
-const { uniqueDonors } = require('../../src');
-const { before } = require('lodash');
+const { uniqueDonors } = require('../../src/uniqueDonors');
 const { expect } = chai;
 // before block to sets off the nocks
 // several it statements to check:
@@ -16,21 +15,59 @@ const { expect } = chai;
 // wrong uuid
 // check for the right respons
 
-describe('wrong campaignUuid', () => {
+describe('unique donors', () => {
 	let patchBody;
 	before(() => {
-		nock(RAISELY_API)
-			.post('/v3/profiles/${UUID}')
-			.reply((uri, body) => {
-				patchBody = body; // to check body contains no duplicated email (donor count)
+		nock.cleanAll();
+		nock('https://api.raisely.com')
+			.get(
+				'/v3/donations?campaign=campaignUuid&profile=profileUuid&private=1'
+			)
+			.reply((uri, requestBody) => {
+				console.log('uri: ', uri);
+				console.log('requestBody: ', requestBody);
+				return [200, {}];
 			});
+
+		const request = prepareUniqueDonorsRequest();
+		return uniqueDonors(request);
 	});
 	// some filler code
 	it('it works', () => {
+		// uniqueDonors(request);
 		const a = 1;
 		expect(a).to.eq(1);
 	});
 });
+
+const prepareUniqueDonorsRequest = () => {
+	return new MockRequest({
+		headers: {
+			origin: 'https://climateconversations.raisely.com',
+		},
+		method: 'POST',
+		url: '/',
+		body: {
+			data: {
+				data: {
+					campaignUuid: 'campaignUuid',
+					profile: {
+						uuid: 'profileUuid',
+					},
+				},
+			},
+		},
+	});
+};
+
+// mock request first
+// should mock the muliple data
+// nock the 2 api calls
+// nock(RAISELY_API)
+// 	.post('/v3/profiles/${UUID}')
+// 	.reply((uri, body) => {
+// 		patchBody = body; // to check body contains no duplicated email (donor count)
+// 	});
 
 // create mock request to pass into uniqueDonors
 // to nock:
