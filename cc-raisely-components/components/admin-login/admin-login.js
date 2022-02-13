dad8c397224a2a716a07741ef9683701900fbd26
KangJingA <66071629+KangJingA@users.noreply.github.com>
2022-02-13 11:52:40 +0800

    refactor class based component into hook

--- cc-raisely-components/components/admin-login/admin-login.js
+++ cc-raisely-components/components/admin-login/admin-login.js
@@ -2,14 +2,37 @@
  * Logs the user in with a full admin login token so they can access all
  * features of the API through the portal
  */
-(RaiselyComponents, React) =>
-	class AdminLogin extends React.Component {
-		render() {
+(RaiselyComponents, React) => {
+	const { useEffect } = React;
+
+	return function AdminLogin(props) {
+		useEffect(() => {
 			if (!document.body.classList.contains('components-loaded')) {
 				document.body.classList.add('components-loaded');
 			}
+		});
 
-			const LoginForm = window.CustomComponentRaiselyLoginForm.html;
-			return <LoginForm requestAdminToken {...this.props} />;
-		}
+		let LoginForm = window.CustomComponentRaiselyLoginForm.html;
+
+		return (
+			<div>
+				<LoginForm requestAdminToken {...props} />
+			</div>
+		);
 	};
+};
+// class AdminLogin extends React.Component {
+// 	render() {
+// 		if (!document.body.classList.contains('components-loaded')) {
+// 			document.body.classList.add('components-loaded');
+// 		}
+
+// 		const LoginForm = window.CustomComponentRaiselyLoginForm.html;
+
+// 		return (
+// 			<div>
+// 				<LoginForm requestAdminToken {...this.props} />
+// 			</div>
+// 		);
+// 	}
+// };
