401bb8060d972a8a26ffb89add4ebceab7223439
KangJingA <66071629+KangJingA@users.noreply.github.com>
2022-03-22 20:50:29 +0800

    Update birthdayFundraiseReminder.spec.js

--- cc-proxy/spec/controllers/birthdayFundraiseReminder.spec.js
+++ cc-proxy/spec/controllers/birthdayFundraiseReminder.spec.js
@@ -1,21 +1,99 @@
 const chai = require('chai');
 const nock = require('nock');
 const _ = require('lodash');
+const dayjs = require('dayjs');
 
 require('../spec.env.js');
 
 const MockRequest = require('../utils/mockRequest');
+const MockResponse = require('../utils/mockResponse');
 
 const {
 	birthdayFundraiseReminder,
 } = require('../../src/birthdayFundraiseReminder');
 
 const { expect } = chai;
+let currentYear = dayjs().get('year');
+
+describe('test', () => {
+	let patchBody;
+
+	before(() => {
+		nock.cleanAll();
+
+		nock('https://api.raisely.com')
+			.get(
+				'/v3/users?private=1&nextBirthdayAbsent=1&dateOfBirthPresent=1'
+			)
+			.reply(200, invalidDateOfBirthResponse);
+		nock('https://api.raisely.com')
+			.get(`/v3/users`)
+			.query({
+				'private': true,
+				'private.nextBirthdayLTE': /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))/,
+			})
+			.reply(200, invalidNextBirthdayResponse);
+	});
+
+	it('emptyResponse', async () => {
+		let mockResponse = new MockResponse();
+		await birthdayFundraiseReminder(new MockRequest(), mockResponse);
+		expect(mockResponse.body).to.eql({
+			nextBirthdayUpdated: [],
+			nextBirthdayPassedUpdated: [],
+		});
+	});
+});
 
 // update nextBirthday only
 // update nextBirthdayUpdate only
 // both
-// none
 // invalid dateOfBirth
 // invalid nextBirthday
-// both invalid
+
+const emptyResponse = {
+	nextBirthdayUpdated: [],
+	nextBirthdayPassedUpdated: [],
+};
+
+const validDateOfBirthResponse = {
+	data: [
+		{
+			private: {
+				dateOfBirth: '1995-05-03T00:00:00.000Z',
+			},
+		},
+	],
+};
+
+const invalidDateOfBirthResponse = {
+	data: [
+		{
+			private: {
+				dateOfBirth: 'invalid date',
+			},
+		},
+	],
+};
+
+const validNextBirthdayResponse = {
+	data: [
+		{
+			private: {
+				nextBirthday: dayjs('1995-05-03T00:00:00.000Z')
+					.set('year', currentYear)
+					.toISOString(),
+			},
+		},
+	],
+};
+
+const invalidNextBirthdayResponse = {
+	data: [
+		{
+			private: {
+				nextBirthday: 'invalid date',
+			},
+		},
+	],
+};
